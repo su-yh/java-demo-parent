7e98dc489b3b4bfdd88ed10110ba386d79489ea8
suyh <su_787910081@sina.com>
2020-07-02 01:14:15 +0800

    Model

---
+++ demo-02-springboot-09-shiro-03/src/main/java/com/suyh/Shiro03Application.java
@@ -0,0 +1,11 @@
+package com.suyh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shiro03Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Shiro03Application.class, args);
+    }
+}
---
+++ demo-02-springboot-09-shiro-03/src/main/java/com/suyh/realms/UserRealm02.java
@@ -0,0 +1,51 @@
+package com.suyh.realms;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.util.ByteSource;
+
+/**
+ * 自定义Realm 的实现
+ */
+public class UserRealm02 extends AuthorizingRealm {
+    /**
+     * 授权
+     *
+     * @param principals
+     * @return
+     */
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+        return null;
+    }
+
+    /**
+     * 认证
+     * 完成身份认证并且返回认证信息
+     * 如果认证失败则返回null
+     *
+     * @param token
+     * @return
+     * @throws AuthenticationException
+     */
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        // 获取用户输入的用户名，其实就是 UsernamePasswordToken::getPrincipal() 即：getUsername();
+        String username = (String) token.getPrincipal();
+        System.out.println("username====" + username);
+        // 根据用户名到数据库中查询密码信息 -- 模拟
+        // 假定从数据库中获取的密码就是: "1111" + "sxt"(盐) + 2(迭代次数)
+        // new Md5Hash("1111", "sxt", 2): "e41cd85110c7533e3f93b729b25235c3"
+        String pwd = "e41cd85110c7533e3f93b729b25235c3";
+        String slat = "sxt";
+        // 将数据库中查询到的信息封装到 SimpleAuthenticationInfo 中
+        SimpleAuthenticationInfo info = new SimpleAuthenticationInfo(
+                username, pwd, ByteSource.Util.bytes(slat), getName());
+        return info;
+    }
+}
