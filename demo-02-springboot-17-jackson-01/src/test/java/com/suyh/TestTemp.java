a6ccd7f5aa78f34c6adc5b9e987675ed15bfe8a9
suyh <su_787910081@sina.com>
2020-07-02 22:37:36 +0800

    Jackson 提取为一个jar 包

--- demo-02-springboot-17-jackson-01/src/test/java/com/suyh/TestTemp.java
+++ demo-02-springboot-17-jackson-01/src/test/java/com/suyh/TestTemp.java
@@ -4,15 +4,10 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.suyh.entity.Person;
 import com.suyh.utils.JsonUtil;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@SpringBootTest(classes = {Application.class})
-@RunWith(SpringRunner.class)
 public class TestTemp {
 
     @Test
