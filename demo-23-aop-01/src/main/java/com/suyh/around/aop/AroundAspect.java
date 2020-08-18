a931f244d7abcb132fe11567ff32bcd8c914f490
suyh <su_787910081@sina.com>
2020-08-18 22:02:15 +0800

    环线增强，记录方法的执行时间

--- demo-23-aop-01/src/main/java/com/suyh/around/aop/AroundAspect.java
+++ demo-23-aop-01/src/main/java/com/suyh/around/aop/AroundAspect.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class AroundAspect {
     // `@Around`：环绕增强，相当于MethodInterceptor
-    @Around("execution(* com.suyh.around.service.*.*(..))")
+    @Around("execution(* com.suyh.around.service.AroundService.*(..))")
     public Object execMethod(ProceedingJoinPoint point) throws Throwable {
         log.info("@Around：执行目标方法之前...");
         // 访问目标方法的参数：
@@ -28,5 +28,19 @@ public class AroundAspect {
         log.info("@Around：执行目标方法之后...");
         log.info("@Around：被织入的目标对象为：" + point.getTarget());
         return "原返回值：" + returnValue + "，这是返回结果的后缀";
+    }
+
+    @Around("@annotation(com.suyh.around.annotation.MethodDuration)")
+    public Object methodTime(ProceedingJoinPoint point) throws Throwable {
+        String className = point.getTarget().getClass().getName();
+        String methodName = point.getSignature().getName();
+        log.info("@Around.methodTime: before, 类【{}】的方法【{}】", className, methodName);
+        // 这里我们没的填写参数，但是不管实际方法有什么参数都会正常传入
+        long beforeMillis = System.currentTimeMillis();
+        Object returnValue = point.proceed();
+        long afterMillis = System.currentTimeMillis();
+        log.info("@Around.methodTime: 类【{}】的方法【{}】，执行总耗时为：【{}ms】",
+                className, methodName, afterMillis - beforeMillis);
+        return returnValue;
     }
 }
---
+++ demo-23-aop-01/src/test/java/com/suyh/TestSayHelloAroundService.java
@@ -0,0 +1,26 @@
+package com.suyh;
+
+import com.suyh.around.service.SayHelloAroundService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = AopApplication.class)
+public class TestSayHelloAroundService {
+    @Resource
+    private SayHelloAroundService service;
+
+    @Test
+    public void test01() {
+        service.sayHelloVoid();
+    }
+
+    @Test
+    public void test02() {
+        service.sayHelloString("test02");
+    }
+}
