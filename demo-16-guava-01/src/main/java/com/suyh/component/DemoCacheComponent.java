8cd729d29b9c3c52dc9fbce0655d1556f28d8fdf
苏云弘 <su_787910081@sina.com>
2020-09-05 20:29:36 +0800

    缓存

--- demo-16-guava-01/src/main/java/com/suyh/component/DemoCacheComponent.java
+++ demo-16-guava-01/src/main/java/com/suyh/component/DemoCacheComponent.java
@@ -13,9 +13,16 @@ import java.util.concurrent.TimeUnit;
 @Component
 public class DemoCacheComponent {
     private Cache<String, Object> commonCache = null;
-
     private Map<String, Object> staticDataSource = null;
 
+    /**
+     * 该注解可以实现在运行工程时，自动运行该注解下的方法；
+     * 是在项目启动的时候执行这个方法，也可以理解为在spring容器启动的时候执行，可作为一些数据的常规化加载，比如数据字典之类的。
+     * 被@PostConstruct修饰的方法会在服务器加载Servle的时候运行，并且只会被服务器执行一次。PostConstruct在构造函数之后执行
+     * 也就是加载顺序
+     * 服务器加载Servlet -> servlet 构造函数的加载 -> postConstruct ->init（init是在service 中的初始化方法. 创建service 时发生的事件.
+     * ->Service->destory->predestory->服务器卸载serlvet
+     */
     @PostConstruct
     public void init() {
         commonCache = CacheBuilder.newBuilder()
@@ -23,7 +30,7 @@ public class DemoCacheComponent {
                 .initialCapacity(5)
                 // 最大缓存容量，超过后会按照LRU 策略-最近最少使用原则，来移除缓存项
                 .maximumSize(10)
-                // 写入之后保留1 分钟时间
+                // 写入之后保留时间，指定时间长度与时间单位
                 .expireAfterWrite(10, TimeUnit.SECONDS)
                 .build();
 
