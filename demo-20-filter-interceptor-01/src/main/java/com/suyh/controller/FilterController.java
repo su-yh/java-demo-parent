e9d97d55790e2b09008d32c79e0daa2062226c08
苏云弘 <su_787910081@sina.com>
2020-09-17 01:35:14 +0800

    logback-spring.xml 日志灵活配置

--- demo-20-filter-interceptor-01/src/main/java/com/suyh/controller/FilterController.java
+++ demo-20-filter-interceptor-01/src/main/java/com/suyh/controller/FilterController.java
@@ -1,33 +1,33 @@
-package com.suyh.controller;
-
-import com.suyh.entity.DemoEntityUser;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Date;
-
-@RestController
-@RequestMapping("/filter")
-@Slf4j
-public class FilterController {
-
-    @GetMapping("/name")
-    public String getName(@RequestParam("name") String name) {
-        log.info("name: {}", name);
-        return "filter: " + name;
-    }
-
-    @GetMapping("/entity")
-    public DemoEntityUser getEntity() {
-        log.info("getEntity");
-        DemoEntityUser res = new DemoEntityUser();
-        res.setName("suyh");
-        res.setAge(10);
-        res.setCreateTime(new Date());
-
-        return res;
-    }
-}
+//package com.suyh.controller;
+//
+//import com.suyh.entity.DemoEntityUser;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.Date;
+//
+//@RestController
+//@RequestMapping("/filter")
+//@Slf4j
+//public class FilterController {
+//
+//    @GetMapping("/name")
+//    public String getName(@RequestParam("name") String name) {
+//        log.info("name: {}", name);
+//        return "filter: " + name;
+//    }
+//
+//    @GetMapping("/entity")
+//    public DemoEntityUser getEntity() {
+//        log.info("getEntity");
+//        DemoEntityUser res = new DemoEntityUser();
+//        res.setName("suyh");
+//        res.setAge(10);
+//        res.setCreateTime(new Date());
+//
+//        return res;
+//    }
+//}
