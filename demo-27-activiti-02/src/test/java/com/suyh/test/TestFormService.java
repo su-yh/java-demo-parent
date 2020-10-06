7894b54bd7c9e9251c99050d24486d72fa845b82
suyh <su_787910081@sina.com>
2020-10-06 18:27:28 +0800

    formservice

--- demo-27-activiti-02/src/test/java/com/suyh/test/TestFormService.java
+++ demo-27-activiti-02/src/test/java/com/suyh/test/TestFormService.java
@@ -3,6 +3,7 @@ package com.suyh.test;
 import lombok.extern.slf4j.Slf4j;
 import org.activiti.engine.FormService;
 import org.activiti.engine.RepositoryService;
+import org.activiti.engine.form.FormProperty;
 import org.activiti.engine.form.StartFormData;
 import org.activiti.engine.repository.ProcessDefinition;
 import org.activiti.engine.test.ActivitiRule;
@@ -11,28 +12,43 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 @Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class TestFormService {
-    @Rule
-    public ActivitiRule activitiRule = new ActivitiRule("activiti_druid.cfg.xml");
+    @Resource
+    private FormService formService;
+    @Resource
+    private RepositoryService repositoryService;
+
     @Test
-    @Deployment(resources = "processes/second_approve.bpmn")
     public void test01() {
-        final String processDefinitionKey = "second_approve";
-        RepositoryService repositoryService = activitiRule.getRepositoryService();
+        final String processDefinitionKey = "my-process";
         List<ProcessDefinition> list = repositoryService.createProcessDefinitionQuery()
-                .processDefinitionKey(processDefinitionKey).list();
-        log.info("size: {}", list.size());
-        if (true) {
+                .processDefinitionKey(processDefinitionKey)
+                // 取版本最高的一个
+                .orderByProcessDefinitionVersion().desc()
+                .listPage(0, 1);
+        if (list == null || list.isEmpty()) {
+            log.warn("list is empty");
             return;
         }
-        FormService formService = activitiRule.getFormService();
-        StartFormData startFormData = formService.getStartFormData("LoanRequestProcess:3:12505");
-
+        ProcessDefinition processDefinition = list.get(0);
+        log.info("processDefinition: {}", ToStringBuilder.reflectionToString(processDefinition, ToStringStyle.JSON_STYLE));
+        StartFormData startFormData = formService.getStartFormData(processDefinition.getId());
 
         log.info("startFormData: {}", ToStringBuilder.reflectionToString(startFormData, ToStringStyle.JSON_STYLE));
+
+        List<FormProperty> formProperties = startFormData.getFormProperties();
+        for (FormProperty formProperty : formProperties) {
+            log.info("formProperty: {}", ToStringBuilder.reflectionToString(formProperty, ToStringStyle.JSON_STYLE));
+        }
     }
 }
