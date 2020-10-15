dd4144270bef08450c1e25d8af6b50655d432079
苏云弘 <su_787910081@sina.com>
2020-10-15 23:39:16 +0800

    jpa

--- demo-29-db-jpa-01/src/main/java/com/suyh/mapper/FormProperTemplateMapper.java
+++ demo-29-db-jpa-01/src/main/java/com/suyh/mapper/FormProperTemplateMapper.java
@@ -10,7 +10,9 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface FormProperTemplateMapper extends JpaRepository<FormPropertyTemplateEntity, Long> {
     /**
-     * TODO: 问题，这个查询出来的是全部数据，并不是分页指定的条数
+     * 问题：这个查询出来的是全部数据，并不是分页指定的条数
+     * `@Query(value = "SELECT model FROM FormPropertyTemplateEntity model WHERE parentId IS NULL ORDER BY ?#{#pageable}")`
+     * 你大爷的，这后面的 "ORDER BY ?#{#pageable}" 不能加，加了就是全部查询出来了，起不到分页的作用。
      *
      * 自定义分页查询
      * 查询某个字段为NULL的情况
@@ -19,7 +21,6 @@ public interface FormProperTemplateMapper extends JpaRepository<FormPropertyTemp
      * @param pageable 分页条件，页数从0 开始
      * @return 返回列表
      */
-    @Query(value = "SELECT model FROM FormPropertyTemplateEntity model WHERE parentId IS NULL ORDER BY ?#{#pageable}",
-            countQuery = "SELECT count(1) FROM FormPropertyTemplateEntity model WHERE parentId IS NULL")
+    @Query(value = "SELECT model FROM FormPropertyTemplateEntity model WHERE parentId IS NULL")
     Page<FormPropertyTemplateEntity> pageQueryParentIsNull(Pageable pageable);
 }
