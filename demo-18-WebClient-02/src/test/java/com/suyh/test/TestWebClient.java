388fa631f9495779f0c61cbd41a5316bb7435221
苏云弘 <su_787910081@sina.com>
2020-10-20 22:37:40 +0800

    httpclient

--- demo-18-WebClient-02/src/test/java/com/suyh/test/TestWebClient.java
+++ demo-18-WebClient-02/src/test/java/com/suyh/test/TestWebClient.java
@@ -6,10 +6,12 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
@@ -18,7 +20,7 @@ public class TestWebClient {
     @Test
     public void test01() throws InterruptedException {
         //创建客户端
-        WebClient client = WebClient.create("http://localhost:8080");
+        WebClient client = WebClient.create("http://localhost:11801");
 
         // subscribe() 就是提交，然后后台就会去自动触发HTTP 请求，这里显然是有一个线程在处理。
         //GET请求用户列表
@@ -50,5 +52,100 @@ public class TestWebClient {
 
         TimeUnit.SECONDS.sleep(10);
         log.info("finished.");
+    }
+
+    /**
+     * Flux 的调用是异常的，不会阻塞当前线程，所以需要用一个策略去让程序继续 执行
+     *
+     * @throws InterruptedException
+     */
+    @Test
+    public void testFluxGetHttp01() throws InterruptedException {
+        //创建客户端
+        WebClient client = WebClient.create("http://localhost:11801");
+
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        // subscribe() 就是提交，然后后台就会去自动触发HTTP 请求，这里显然是有一个线程在处理。
+        // Notice.class 为响应的返回类型实体定义
+        Flux<Notice> noticeFlux = client.get().uri("/impl/get/info").retrieve().bodyToFlux(Notice.class);
+        noticeFlux.subscribe(
+                // 正常结束，处理返回值
+                notice -> log.info("noticeFlux success, response entity: {}",
+                        ToStringBuilder.reflectionToString(notice, ToStringStyle.JSON_STYLE)),
+                // 出异常时调用
+                exception -> {
+                    countDownLatch.countDown();
+                    log.error("noticeFlux exception", exception);
+                },
+                // 如果异常，则没有调用
+                () -> {
+                    countDownLatch.countDown();
+                    log.info("noticeFlux complete.");
+                });
+
+        countDownLatch.await();
+        log.info("noticeFlux is finished.");
+        TimeUnit.SECONDS.sleep(5);
+    }
+
+    /**
+     * Mono 的请求是阻塞线程，立即调用，直到调用响应返回结果为止.
+     * 但是这样有一个问题就是当出现访问报错的情况就不好处理。
+     */
+    @Test
+    public void testMonoGetHttp01() {
+        //创建客户端
+        WebClient client = WebClient.create("http://localhost:11801");
+
+        // Notice.class 为响应的返回类型实体定义
+        Mono<Notice> noticeMono = client.get().uri("/impl/get/info").retrieve().bodyToMono(Notice.class);
+        Notice resultEntity = noticeMono.block();   // 这里会阻塞当前线程，直到请求返回结果
+        log.info("noticeMono success, response entity: {}",
+                ToStringBuilder.reflectionToString(resultEntity, ToStringStyle.JSON_STYLE));
+    }
+
+    @Test
+    public void testGetHttp() {
+        //创建客户端
+        WebClient client = WebClient.create("http://localhost:11801/");
+
+        // Notice.class 为响应的返回类型实体定义
+        ClientResponse clientResponse = client.get().uri("/impl/get/info").exchange().block();
+        HttpStatus httpStatus = clientResponse.statusCode();
+        if (httpStatus.equals(HttpStatus.OK)) {
+            // 这里的结果只能拿一次，要么使用Mono，要么使用Flux，否则会报异常的
+            if (true) {
+                Flux<Notice> resultEntityFlux = clientResponse.bodyToFlux(Notice.class);
+                Notice noticeEntityFlux = resultEntityFlux.blockFirst();
+                log.info("noticeMono success, response resultEntityFlux: {}",
+                        ToStringBuilder.reflectionToString(noticeEntityFlux, ToStringStyle.JSON_STYLE));
+            } else {
+                Mono<Notice> resultEntityMono = clientResponse.bodyToMono(Notice.class);
+                Notice noticeEntityMono = resultEntityMono.block();
+                log.info("noticeMono success, response resultEntityMono: {}",
+                        ToStringBuilder.reflectionToString(noticeEntityMono, ToStringStyle.JSON_STYLE));
+            }
+        } else {
+            log.error("noticeMono failed, HttpStatus: {}", httpStatus);
+        }
+    }
+
+    @Test
+    public void testPostHttp() throws InterruptedException {
+        Notice noticeEntity = new Notice();
+        noticeEntity.setStatus(10086);
+        Flux<Notice> noticePostFlux = WebClient.create("http://localhost:11801").post().uri("/impl/post/info/entity")
+                // 给出请求body 的值
+                .bodyValue(noticeEntity).retrieve()
+                // 对异常状态的处理，对于状态为200 的不需要处理。如果有多个，则链式写法就可以了
+                // 这里如果捕获到了，那么后面的subscribe 里面就不会被调用了。
+                .onStatus(httpStatus -> httpStatus.equals(HttpStatus.NOT_FOUND), clientResponse -> Mono.error(new RuntimeException("error 404")))
+                .onStatus(httpStatus -> httpStatus.equals(HttpStatus.INTERNAL_SERVER_ERROR), clientResponse -> Mono.error(new RuntimeException("error 500")))
+                .bodyToFlux(Notice.class);
+        noticePostFlux.subscribe(notice -> log.info("noticeFlux 10086: {}",
+                ToStringBuilder.reflectionToString(notice, ToStringStyle.JSON_STYLE)), error -> log.error("error", error));
+
+        log.info("http request finished.");
+        TimeUnit.SECONDS.sleep(4);
     }
 }
