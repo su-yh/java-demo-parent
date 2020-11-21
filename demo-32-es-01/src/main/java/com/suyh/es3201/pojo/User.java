c3472658a0741762ceb78e7acb1564d0b0b012ee
苏云弘 <su_787910081@sina.com>
2020-11-21 17:28:50 +0800

    ES

---
+++ demo-32-es-01/src/main/java/com/suyh/es3201/pojo/User.java
@@ -0,0 +1,13 @@
+package com.suyh.es3201.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private String name;
+    private int age;
+}
--- demo-32-es-01/src/test/java/com/suyh/es3201/Application3201Tests.java
+++ demo-32-es-01/src/test/java/com/suyh/es3201/Application3201Tests.java
@@ -1,10 +1,37 @@
 package com.suyh.es3201;
 
+import com.suyh.es3201.pojo.User;
+import com.suyh.utils.JsonUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.support.master.AcknowledgedResponse;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CreateIndexRequest;
 import org.elasticsearch.client.indices.CreateIndexResponse;
+import org.elasticsearch.client.indices.GetIndexRequest;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.MatchAllQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.TermQueryBuilder;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.FetchSourceContext;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,6 +39,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
 import java.io.IOException;
+import java.util.ArrayList;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application3201.class)
@@ -28,5 +56,128 @@ public class Application3201Tests {
         // 执行创建命令
         CreateIndexResponse createIndexResponse = client.indices().create(request, RequestOptions.DEFAULT);
         log.info(createIndexResponse.toString());
+    }
+
+    // 获取索引，索引只能判断其是否存在
+    @Test
+    public void testExistIndex() throws IOException {
+        GetIndexRequest request = new GetIndexRequest("kuang_index");
+        boolean exists = client.indices().exists(request, RequestOptions.DEFAULT);
+        log.info("exists: {}", exists);
+    }
+
+    // 删除索引
+    @Test
+    public void testDeleteIndex() throws IOException {
+        DeleteIndexRequest request = new DeleteIndexRequest("kuang_index");
+        AcknowledgedResponse response = client.indices().delete(request, RequestOptions.DEFAULT);
+        log.info("delete result: {}", response.isAcknowledged());
+    }
+
+    // 添加文档
+    @Test
+    public void testAddDocument() throws IOException {
+        User user = new User("狂神说", 3);
+        IndexRequest request = new IndexRequest("kuang_index");
+
+        // 规则 put /kuang_index/_doc/1
+        request.id("1");
+        request.timeout(TimeValue.timeValueSeconds(1));
+        request.timeout("1s");
+
+        // 将数据放入请求 json
+        request.source(JsonUtil.serializable(user), XContentType.JSON);
+
+        // 发请求求
+        IndexResponse response = client.index(request, RequestOptions.DEFAULT);
+        log.info(response.toString());
+    }
+
+    // 获取文档，判断是否存在 get /index/_doc/1
+    @Test
+    public void testIsExists() throws IOException {
+        GetRequest request = new GetRequest("kuang_index", "1");
+        // 不获取返回的 _source 的上下文数据
+        request.fetchSourceContext(new FetchSourceContext(false));
+        // 排序字段
+        request.storedFields("_none_");
+
+        boolean exists = client.exists(request, RequestOptions.DEFAULT);
+        log.info("result: {}", exists);
+    }
+
+    // 获取文档信息
+    @Test
+    public void testGetDoc() throws IOException {
+        GetRequest request = new GetRequest("kuang_index", "1");
+        GetResponse response = client.get(request, RequestOptions.DEFAULT);
+        log.info("result: {}", response.getSourceAsString());
+        log.info("result: {}", response);   // 返回的全部内容和命令是一样的
+    }
+
+    // 更新文档信息
+    @Test
+    public void testUpdateDoc() throws IOException {
+        UpdateRequest request = new UpdateRequest("kuang_index", "1");
+        request.timeout(TimeValue.timeValueSeconds(1));
+
+        User user = new User("狂神说Java", 18);
+        request.doc(JsonUtil.serializable(user), XContentType.JSON);
+
+        UpdateResponse response = client.update(request, RequestOptions.DEFAULT);
+        log.info("result: {}", response.status());
+        log.info("result: {}", response);   // 返回的全部内容和命令是一样的
+    }
+
+    // 删除文档记录
+    @Test
+    public void testDeleteDoc() throws IOException {
+        DeleteRequest request = new DeleteRequest("kuang_index", "3");
+
+        DeleteResponse response = client.delete(request, RequestOptions.DEFAULT);
+        log.info("result:{}", response.status());
+        log.info("result: {}", response);
+    }
+
+    @Test
+    public void testBulkDoc() throws IOException {
+        BulkRequest bulkRequest = new BulkRequest();
+        bulkRequest.timeout(TimeValue.timeValueSeconds(10));
+
+        ArrayList<User> userList = new ArrayList<>();
+        userList.add(new User("kuangeshen1", 3));
+
+        for (int i = 0; i < userList.size(); i++) {
+            bulkRequest.add(new IndexRequest("kuange_index").id("" + (i + 1))
+                    .source(JsonUtil.serializable(userList.get(i)), XContentType.JSON));
+        }
+
+        BulkResponse response = client.bulk(bulkRequest, RequestOptions.DEFAULT);
+        log.info("result: {}", response);
+    }
+
+    // 查询
+    @Test
+    public void testSearch() throws IOException {
+        SearchRequest request = new SearchRequest("kuange_index");
+        // 构建搜索条件
+//        MatchAllQueryBuilder matchAllQueryBuilder = QueryBuilders.matchAllQuery();
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        TermQueryBuilder termQueryBuilder = QueryBuilders.termQuery("name", "kuangeshen1");
+        sourceBuilder.query(termQueryBuilder);
+        // 分页查询
+        sourceBuilder.from(0);
+        sourceBuilder.size(10);
+
+        // 将搜索条件放到查询对象中
+        request.source(sourceBuilder);
+
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        // 得到搜索的结果，全部在hits 中
+        SearchHits hits = response.getHits();
+        log.info("hits: {}", hits);
+        for (SearchHit documentField : hits) {
+            log.info("documentField: {}", documentField.getSourceAsMap());
+        }
     }
 }
