8f12f71013f63677ce640218fb9f62051a37857d
苏云弘 <su_787910081@sina.com>
2020-12-05 20:08:01 +0800

    es

--- demo-32-es-02/src/main/java/com/suyh/es3202/config/EsCustomRepositoryConfiguration.java
+++ demo-32-es-02/src/main/java/com/suyh/es3202/config/EsCustomRepositoryConfiguration.java
@@ -3,10 +3,8 @@ package com.suyh.es3202.config;
 import com.suyh.es3202.es.CustomEsEntityMapper;
 import com.suyh.es3202.es.CustomEsResultMapper;
 import com.suyh.es3202.es.CustomRepositoryImpl;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.EntityMapper;
 import org.springframework.data.elasticsearch.core.ResultsMapper;
 import org.springframework.data.elasticsearch.core.mapping.SimpleElasticsearchMappingContext;
@@ -15,7 +13,6 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 /**
  * 按配置来进行添加自定义repository
  */
-@ConditionalOnBean(value = {ElasticsearchOperations.class}, name = {"elasticsearchTemplate"})
 @Configuration
 @EnableElasticsearchRepositories(basePackages = "com.suyh.es3202.repository", repositoryBaseClass = CustomRepositoryImpl.class)
 public class EsCustomRepositoryConfiguration {
