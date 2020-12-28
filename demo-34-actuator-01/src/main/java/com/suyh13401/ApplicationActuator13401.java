7a6e89d22f62383915c7fc935d8bee04c2dc05fe
苏云弘 <su_787910081@sina.com>
2020-12-29 00:21:04 +0800

    spring-boot-actuator

---
+++ demo-34-actuator-01/src/main/java/com/suyh13401/ApplicationActuator13401.java
@@ -0,0 +1,11 @@
+package com.suyh13401;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationActuator13401 {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationActuator13401.class, args);
+    }
+}
