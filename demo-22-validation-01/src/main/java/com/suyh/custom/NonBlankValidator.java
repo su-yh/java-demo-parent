3f1e2d551e347c1e3eb5c4b3d4c0c31bdaccfdf5
su787910081 <su_787910081@sina.com>
2021-04-02 08:53:15 +0800

    Create NonBlankValidator.java

---
+++ demo-22-validation-01/src/main/java/com/suyh/custom/NonBlankValidator.java
@@ -0,0 +1,26 @@
+package com.suyh.custom.validation.constraints;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+/**
+ * 自定义实现接口参数校验注解对字符串的功能实现
+ * 允许null值，但是不允许元素空
+ *
+ * @author 苏雲弘
+ * @since 2021-03-25
+ */
+public class NonBlankValidator implements ConstraintValidator<NonBlank, CharSequence> {
+    @Override
+    public void initialize(NonBlank constraintAnnotation) {
+    }
+
+    @Override
+    public boolean isValid(CharSequence value, ConstraintValidatorContext context) {
+        if (value == null) {
+            return true;
+        }
+
+        return value.toString().trim().length() > 0;
+    }
+}
