61d2c475d969e8e42c1219ea66ec1b13b5bd3c7b
su787910081 <su_787910081@sina.com>
2021-04-19 13:57:56 +0800

    str trim

--- demo-00-00-core/src/main/java/com/suyh/utils/ClassUtils.java
+++ demo-00-00-core/src/main/java/com/suyh/utils/ClassUtils.java
@@ -4,6 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Map;
 
 @Slf4j
 public class ClassUtils {
@@ -36,5 +39,61 @@ public class ClassUtils {
                 throw new RuntimeException("propertyInject failed");
             }
         }
+    }
+
+    public static <T> void strTrim(T entity, Class<?> clazz) {
+        if (entity == null || clazz == null) {
+            return;
+        }
+
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field field : fields) {
+            if (Modifier.isStatic(field.getModifiers())) {
+                continue;
+            }
+
+            try {
+                field.setAccessible(true);
+                Object fieldValue = field.get(entity);
+                if (fieldValue == null) {
+                    continue;
+                }
+
+                if (fieldValue instanceof String) {
+                    String strValue = ((String) fieldValue).trim();
+                    field.set(entity, strValue);
+                    log.info("field trim, class: {}, name: {} value: {}, type: {}",
+                            clazz.getSimpleName(), field.getName(), field.get(entity), field.getType());
+                } else if (fieldValue instanceof Map) {
+                    Map<Object, Object> mapValue = (Map<Object, Object>) fieldValue;
+                    Collection<Object> values = mapValue.values();
+                    for (Object obj : values) {
+                        strTrim(obj, obj.getClass());
+                    }
+                } else if (fieldValue instanceof Collection) {
+                    Collection<Object> collObject = (Collection<Object>) fieldValue;
+                    for (Object obj : collObject) {
+                        strTrim(obj, obj.getClass());
+                    }
+                } else if (fieldValue instanceof Integer
+                        || fieldValue instanceof Boolean
+                        || fieldValue instanceof Long
+                        || fieldValue instanceof Double
+                        || fieldValue instanceof Float
+                        || fieldValue instanceof Date
+                        || fieldValue instanceof Byte) {
+                    continue;
+                } else {
+                    // 剩下的应该就是自定义类型了吧
+                    // String fieldValue = (String) field.get(entity);
+                    strTrim(fieldValue, fieldValue.getClass());
+                }
+            } catch (IllegalAccessException e) {
+                log.warn("strTrim failed", e);
+            }
+        }
+
+        // 父类中的字段属性
+        strTrim(entity, clazz.getSuperclass());
     }
 }
