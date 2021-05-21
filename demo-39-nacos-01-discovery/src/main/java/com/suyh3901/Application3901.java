ff5527c820aeea2a4f3c8d080f5668d180a34e2f
su787910081 <su_787910081@sina.com>
2021-05-21 09:28:06 +0800

    Create Application3901.java

---
+++ demo-39-nacos-01-discovery/src/main/java/com/suyh3901/Application3901.java
@@ -0,0 +1,11 @@
+package com.suyh3901;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application3901 {
+    public static void main(String[] args) {
+        SpringApplication.run(Application3901.class, args);
+    }
+}
