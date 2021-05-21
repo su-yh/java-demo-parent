8c41a4557c5d959eafc226288d19760ebeba3a94
su787910081 <su_787910081@sina.com>
2021-05-21 09:30:12 +0800

    Create Application3902.java

---
+++ demo-39-nacos-02-registry/src/main/java/com/suyh3902/Application3902.java
@@ -0,0 +1,11 @@
+package com.suyh3902;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application3902 {
+    public static void main(String[] args) {
+        SpringApplication.run(Application3902.class, args);
+    }
+}
