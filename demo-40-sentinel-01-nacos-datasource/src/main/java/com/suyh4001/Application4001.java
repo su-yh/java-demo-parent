3c35910831d9dc4c4f180e6cb45b52a4c48c4bb9
su787910081 <su_787910081@sina.com>
2021-05-25 10:48:22 +0800

    Update Application4001.java

--- demo-40-sentinel-01-nacos-datasource/src/main/java/com/suyh4001/Application4001.java
+++ demo-40-sentinel-01-nacos-datasource/src/main/java/com/suyh4001/Application4001.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application4001 {
     public static void main(String[] args) {
+        // suyh - 心跳发送间隔时间，单位: ms
+        System.setProperty("csp.sentinel.heartbeat.interval.ms","10000");
         SpringApplication.run(Application4001.class, args);
     }
 }
