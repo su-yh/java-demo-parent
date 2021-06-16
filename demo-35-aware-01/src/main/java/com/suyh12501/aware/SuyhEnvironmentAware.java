c93ad4befcbffb7ad39af8ecde49e77ba3f10169
苏云弘 <su_787910081@sina.com>
2021-06-16 21:48:47 +0800

    配置属性，环境变量

--- demo-35-aware-01/src/main/java/com/suyh12501/aware/SuyhEnvironmentAware.java
+++ demo-35-aware-01/src/main/java/com/suyh12501/aware/SuyhEnvironmentAware.java
@@ -1,14 +1,23 @@
 package com.suyh12501.aware;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
-public class SuyhEnvironmentAware implements EnvironmentAware {
+public class SuyhEnvironmentAware implements EnvironmentAware, InitializingBean {
     private Environment environment;
+
+    // 可以通过这样的该获取环境变量的值
+    @Value("${MAVEN_HOME:no_maven_home")
+    private String mavenHome;
+
+    @Value("${windir:nowindir")
+    private String windir;
 
     @Override
     public void setEnvironment(Environment environment) {
@@ -17,5 +26,11 @@ public class SuyhEnvironmentAware implements EnvironmentAware {
         String windirSystemValue = environment.getProperty("windir");
         log.info("MAVEN_HOME: {}", mavenHome);
         log.info("windir: {}", windirSystemValue);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        log.info("init, MAVEN_HOME: {}", mavenHome);
+        log.info("init, windir: {}", windir);
     }
 }
