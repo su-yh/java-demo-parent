52d5c3ef8f67cc7dbae8d6f8f9895aed1259155b
su787910081 <su_787910081@sina.com>
2021-06-19 18:13:27 +0800

    Create Application3903.java

---
+++ demo-39-nacos-03-listener/com/suyh3903/Application3903.java
@@ -0,0 +1,13 @@
+
+package com.suyh3903;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Application3903 {
+    public static void main(String[] args) {
+        SpringApplication.run(Application3903.class, args);
+    }
+}
