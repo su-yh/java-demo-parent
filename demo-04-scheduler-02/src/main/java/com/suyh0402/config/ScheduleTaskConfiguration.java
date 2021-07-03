c7500b230c78ef6b0fefab956357580b0ca64aff
苏云弘 <su_787910081@sina.com>
2021-07-03 12:16:42 +0800

    spring-boot schedule configuration

--- demo-04-scheduler-02/src/main/java/com/suyh0402/config/ScheduleTaskConfiguration.java
+++ demo-04-scheduler-02/src/main/java/com/suyh0402/config/ScheduleTaskConfiguration.java
@@ -1,45 +1,38 @@
-package com.suyh0402.schedule;
+package com.suyh0402.config;
 
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.config.IntervalTask;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 import org.springframework.scheduling.config.TriggerTask;
 import org.springframework.scheduling.support.PeriodicTrigger;
+import org.springframework.stereotype.Component;
 
-import javax.annotation.Resource;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 
-@Configuration
+// 不要遗漏注解 @EnableScheduling，否则定时任务将不生效。
+@Component
 @EnableScheduling
 @Slf4j
-public class ScheduleTimerTaskConfiguration implements SchedulingConfigurer {
-
-    @Bean("taskScheduler")
-    public Executor taskScheduler() {
-        ThreadFactory factory = new ThreadFactoryBuilder().setNameFormat("suyh-demo-pool-%d").build();
-        return Executors.newScheduledThreadPool(5, factory);
-    }
-
-    @Resource(name = "taskScheduler")
-    private Executor executor;
-
+public class ScheduleTaskConfiguration implements SchedulingConfigurer {
     @Override
     public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
-        // 设定一个长度10的定时任务线程池
+        ThreadFactory factory = new ThreadFactoryBuilder().setNameFormat("suyh-demo-pool-%d").build();
+        Executor executor = Executors.newScheduledThreadPool(5, factory);
+        // 设定一个长度固定的定时任务线程池
         taskRegistrar.setScheduler(executor);
+
         // 在这里可以直接添加任务以及cron 表达式
         taskRegistrar.addCronTask(() -> log.info("info - BeansConfiguration"), "* * * * * *");
+
         // 添加一个普通定时任务，首次执行延迟60 秒，之后每5 秒执行一次
         taskRegistrar.addFixedDelayTask(
-                new IntervalTask(this::someTask, 5000, 60000)
+                new IntervalTask(this::someTask, 5000, 20000)
         );
 
         // 两次任务间隔5 秒，对间隔的解释如下：
