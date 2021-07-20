98afdee3a0181b59120ba41a16ee0115a17c9385
su787910081 <su_787910081@sina.com>
2021-07-20 14:25:40 +0800

    Create Application0003.java

---
+++ demo-00-03-constants/src/main/java/com/suyh0003/Application0003.java
@@ -0,0 +1,12 @@
+package com.suyh0003;
+
+public class Application0003 {
+    public static void main(String[] args) {
+        // \r\n 或者 \n
+        System.out.println("换行符：" + System.lineSeparator());
+        // / 或者 \
+        System.out.println("路径分隔符：" + File.separator);
+        // ;
+        System.out.println("File.pathSeparator: " + File.pathSeparator);
+    }
+}
