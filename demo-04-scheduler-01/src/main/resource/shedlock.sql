dd29c9bb99176591431536e37fb407f09ec2773b
su787910081 <su_787910081@sina.com>
2021-09-18 21:57:44 +0800

    Create shedlock.sql

---
+++ demo-04-scheduler-01/src/main/resource/shedlock.sql
@@ -0,0 +1,8 @@
+-- 分布式锁，需要添加如下表到数据库中
+CREATE TABLE shedlock(
+                         name VARCHAR(64),
+                         lock_until TIMESTAMP(3) NULL,
+                         locked_at TIMESTAMP(3) NULL,
+                         locked_by  VARCHAR(255),
+                         PRIMARY KEY (name)
+)
