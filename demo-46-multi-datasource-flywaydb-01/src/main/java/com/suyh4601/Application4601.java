89aaa9bc4cea7dd931620f6175974c9a48c405fb
suyh <su_787910081@sina.com>
2021-11-05 19:29:26 +0800

    Create Application4601.java

---
+++ demo-46-multi-datasource-flywaydb-01/src/main/java/com/suyh4601/Application4601.java
@@ -0,0 +1,13 @@
+package com.suyh4601;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application4601 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application4601.class, args);
+    }
+
+}
