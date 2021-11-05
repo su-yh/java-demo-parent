968292680f8c8c44bb7c9dcf0cfe386763df4e43
suyh <su_787910081@sina.com>
2021-11-05 19:34:31 +0800

    Create SlaveTestUserMapper.java

---
+++ demo-46-multi-datasource-flywaydb-01/src/main/java/com/suyh4601/mapper/slave/SlaveTestUserMapper.java
@@ -0,0 +1,12 @@
+package com.suyh4601.mapper.slave;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.suyh4601.entity.slave.SlaveTestUser;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SlaveTestUserMapper extends BaseMapper<SlaveTestUser> {
+
+
+
+}
