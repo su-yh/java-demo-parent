29b53a87669294ea8c5f23b20a1a4cad906020fd
苏云弘 <su_787910081@sina.com>
2021-11-10 00:35:37 +0800

    多数据源，使用druid 连接池。

--- demo-46-multi-datasource-flywaydb-01/src/main/java/com/suyh4601/config/DatasourceConfig.java
+++ demo-46-multi-datasource-flywaydb-01/src/main/java/com/suyh4601/config/DatasourceConfig.java
@@ -1,24 +1,40 @@
 package com.suyh4601.config;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
 
 @Configuration
-@PropertySource("classpath:config/jdbc.properties")
 public class DatasourceConfig {
     @Bean("dataSourceMaster")
+    @ConfigurationProperties(prefix = "multi4601.datasource.druid.master")
+    @ConditionalOnProperty(name = "multi.datasource.druid.enabled", matchIfMissing = true, havingValue = "true")
+    public DataSource masterDruidDataSource(){
+        return DruidDataSourceBuilder.create().build();
+    }
+
+    @Bean("dataSourceSlave")
+    @ConfigurationProperties(prefix = "multi4601.datasource.druid.slave")
+    @ConditionalOnProperty(name = "multi.datasource.druid.enabled", matchIfMissing = true, havingValue = "true")
+    public DataSource slaveDruidDataSource(){
+        return DruidDataSourceBuilder.create().build();
+    }
+
+    @Bean("dataSourceMaster")
     @ConfigurationProperties(prefix = "multi4601.datasource.master")
+    @ConditionalOnProperty(name = "multi.datasource.druid.enabled", matchIfMissing = true, havingValue = "false")
     public DataSource masterDataSource(){
         return DataSourceBuilder.create().build();
     }
 
     @Bean("dataSourceSlave")
     @ConfigurationProperties(prefix = "multi4601.datasource.slave")
+    @ConditionalOnProperty(name = "multi.datasource.druid.enabled", matchIfMissing = true, havingValue = "false")
     public DataSource slaveDataSource(){
         return DataSourceBuilder.create().build();
     }
