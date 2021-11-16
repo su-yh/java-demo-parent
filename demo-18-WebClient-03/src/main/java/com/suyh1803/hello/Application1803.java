aad730dc95274a09cd5666759f7558ea64d5bcd9
苏云弘 <su_787910081@sina.com>
2021-11-17 00:17:51 +0800

    spring 官网上找的一个示例代码。

---
+++ demo-18-WebClient-03/src/main/java/com/suyh1803/hello/Application1803.java
@@ -0,0 +1,16 @@
+package com.suyh1803.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class Application1803 {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = SpringApplication.run(Application1803.class, args);
+		GreetingClient greetingClient = context.getBean(GreetingClient.class);
+		// We need to block for the content here or the JVM might exit before the message is logged
+		System.out.println(">> message = " + greetingClient.getMessage().block());
+	}
+}
---
+++ demo-18-WebClient-03/src/main/java/com/suyh1803/hello/Greeting.java
@@ -0,0 +1,29 @@
+package com.suyh1803.hello;
+
+
+public class Greeting {
+
+	private String message;
+
+	public Greeting() {
+	}
+
+	public Greeting(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return this.message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "Greeting{" +
+				"message='" + message + '\'' +
+				'}';
+	}
+}
