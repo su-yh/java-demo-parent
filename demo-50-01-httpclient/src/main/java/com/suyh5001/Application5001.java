7ca473c91356d4db001e8fd1ed3f708c5ae0d5b2
苏云弘 <su_787910081@sina.com>
2021-12-04 01:25:56 +0800

    pom.xml

---
+++ demo-50-01-httpclient/src/main/java/com/suyh5001/Application5001.java
@@ -0,0 +1,11 @@
+package com.suyh5001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application5001 {
+    public static void main(String[] args) {
+        SpringApplication.run(Application5001.class, args);
+    }
+}
