1359b3b3c7dc50af75f82d8b61e32509f2e175f5
苏云弘 <su_787910081@sina.com>
2021-12-27 00:23:06 +0800

    数据源

--- demo-46-multi-datasource-flywaydb-01/src/main/java/com/suyh4601/config/DatasourceConfig.java
+++ demo-46-multi-datasource-flywaydb-01/src/main/java/com/suyh4601/config/DatasourceConfig.java
@@ -1,42 +1,47 @@
 package com.suyh4601.config;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
-
 @Configuration
 public class DatasourceConfig {
-    @Bean("dataSourceMaster")
-    @ConfigurationProperties(prefix = "multi4601.datasource.druid.master")
-    @ConditionalOnProperty(name = "multi.datasource.druid.enabled", matchIfMissing = true, havingValue = "true")
-    public DataSource masterDruidDataSource() {
-        return DruidDataSourceBuilder.create().build();
+    @Configuration
+    @ConditionalOnProperty(name = "multi4601.datasource.type", havingValue = "com.alibaba.druid.pool.DruidDataSource")
+    public static class Druid {
+        // 使用druid 提供的builder 构造
+        @Bean("dataSourceMaster")
+        @ConfigurationProperties(prefix = "multi4601.datasource.druid.master")
+        public DruidDataSource masterDruidDataSource() {
+            return DruidDataSourceBuilder.create().build();
+        }
+
+        // 使用spring 提供的builder 构造
+        @Bean("dataSourceSlave")
+        @ConfigurationProperties(prefix = "multi4601.datasource.druid.slave")
+        public DruidDataSource slaveDruidDataSource() {
+            return DataSourceBuilder.create().type(DruidDataSource.class).build();
+        }
     }
 
-    @Bean("dataSourceSlave")
-    @ConfigurationProperties(prefix = "multi4601.datasource.druid.slave")
-    @ConditionalOnProperty(name = "multi.datasource.druid.enabled", matchIfMissing = true, havingValue = "true")
-    public DataSource slaveDruidDataSource() {
-        return DruidDataSourceBuilder.create().build();
-    }
+    @Configuration
+    @ConditionalOnProperty(name = "multi4601.datasource.type", havingValue = "com.zaxxer.hikari.HikariDataSource")
+    public static class Hikari {
+        @Bean("dataSourceMaster")
+        @ConfigurationProperties(prefix = "multi4601.datasource.hikari.master")
+        public HikariDataSource masterDruidDataSource() {
+            return DataSourceBuilder.create().type(HikariDataSource.class).build();
+        }
 
-    @Bean("dataSourceMaster")
-    @ConfigurationProperties(prefix = "multi4601.datasource.master")
-    @ConditionalOnProperty(name = "multi.datasource.druid.enabled", matchIfMissing = true, havingValue = "false")
-    public DataSource masterDataSource() {
-        return DataSourceBuilder.create().build();
+        @Bean("dataSourceSlave")
+        @ConfigurationProperties(prefix = "multi4601.datasource.hikari.slave")
+        public HikariDataSource slaveDruidDataSource() {
+            return DataSourceBuilder.create().type(HikariDataSource.class).build();
+        }
     }
-
-    @Bean("dataSourceSlave")
-    @ConfigurationProperties(prefix = "multi4601.datasource.slave")
-    @ConditionalOnProperty(name = "multi.datasource.druid.enabled", matchIfMissing = true, havingValue = "false")
-    public DataSource slaveDataSource() {
-        return DataSourceBuilder.create().build();
-    }
-
 }
