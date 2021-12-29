7b34e4864f79da0c80c4464898dfcf7ca455e3c5
苏云弘 <su_787910081@sina.com>
2021-12-29 22:41:07 +0800

    自定义actuator

---
+++ demo-34-actuator-01/src/main/java/com/suyh13401/custom/CustomEndpoint.java
@@ -0,0 +1,16 @@
+package com.suyh13401.custom;
+
+import org.springframework.boot.actuate.endpoint.web.annotation.RestControllerEndpoint;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Component
+@RestControllerEndpoint(id = "suyh-custom")
+public class CustomEndpoint {
+    @RequestMapping(value = "/name/{name}", method = RequestMethod.GET)
+    public String name(@PathVariable("name") String name) {
+        return name;
+    }
+}
