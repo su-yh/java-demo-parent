525a1ca304a8a4a27931d1509dbba70cd8108195
苏云弘 <su_787910081@sina.com>
2021-12-29 22:08:13 +0800

    数据源

--- demo-46-multi-datasource-flywaydb-01/src/main/java/com/suyh4601/config/DatasourceConfig.java
+++ demo-46-multi-datasource-flywaydb-01/src/main/java/com/suyh4601/config/DatasourceConfig.java
@@ -3,6 +3,7 @@ package com.suyh4601.config;
 import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
@@ -12,11 +13,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DatasourceConfig {
     @Configuration
+    @ConditionalOnClass(DruidDataSource.class)
     @ConditionalOnProperty(name = "multi4601.datasource.type", havingValue = "com.alibaba.druid.pool.DruidDataSource")
     public static class Druid {
         /**
          * 使用druid 提供的builder 构造
-         * 这里使用的注释@ConfigurationProperties 可以绑定配置属性
+         * 这里使用的注释{@link ConfigurationProperties} 可以绑定配置属性
          * 在bean 对象仓库之后会处理配置项，所以这些配置会被绑定到DruidDataSource。
          */
         @Bean("dataSourceMaster")
--- maven-plugins-usage.md
+++ maven-plugins-usage.md
@@ -6,3 +6,23 @@
 https://blog.csdn.net/shenchaohao12321/article/details/79302791
 
 https://www.cnblogs.com/cowboys/p/10400784.html
+
+
+```xml
+        <!-- provided: 该依赖只在这里使用，子项目可以不使用该依赖中的任何类 -->
+        <dependency>
+            <groupId>org.springframework.boot</groupId>
+            <artifactId>spring-boot-configuration-processor</artifactId>
+            <scope>provided</scope>
+        </dependency>
+```
+
+```xml
+        <!-- compile + true: 子类中如果没有添加这个依赖则会报某个类不存在异常。 -->
+        <dependency>
+            <groupId>org.springframework.boot</groupId>
+            <artifactId>spring-boot-configuration-processor</artifactId>
+            <scope>compile</scope>
+            <optional>true</optional>
+        </dependency>
+```
