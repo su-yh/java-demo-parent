f9b64be13dd086b6769fd44aa7e1a921c7833b67
suyh <su_787910081@sina.com>
2022-01-02 12:39:19 +0800

    spring

---
+++ demo-34-actuator-02/src/main/java/com/suyh3402/Application3402.java
@@ -0,0 +1,11 @@
+package com.suyh3402;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application3402 {
+    public static void main(String[] args) {
+        SpringApplication.run(Application3402.class, args);
+    }
+}
---
+++ demo-34-actuator-02/src/main/resources/db/migration/V1_3__CREATE_TABLE.sql
@@ -0,0 +1,6 @@
+	create table person(
+		name varchar(16),
+		age int
+	) engine=innodb
+	charset=utf8
+	;
