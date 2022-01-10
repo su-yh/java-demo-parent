efd1f7b16d77fa5c0fc8f5851595cd5b94aaef1b
苏云弘 <su_787910081@sina.com>
2022-01-10 23:34:14 +0800

    动态注册bean 对象

--- demo-51-01-bean-dynamic/src/main/java/com/suyh5101/Application5101.java
+++ demo-51-01-bean-dynamic/src/main/java/com/suyh5101/Application5101.java
@@ -2,12 +2,10 @@ package com.suyh5101;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application5101 {
     public static void main(String[] args) {
-        ApplicationContext app = SpringApplication.run(Application5101.class, args);
-        SpringContextUtil.setApplicationContext(app);
+        SpringApplication.run(Application5101.class, args);
     }
 }
--- demo-51-01-bean-dynamic/src/main/java/com/suyh5101/TempDemoController.java
+++ demo-51-01-bean-dynamic/src/main/java/com/suyh5101/TempDemoController.java
@@ -3,17 +3,23 @@ package com.suyh5101;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
 @Slf4j
 public class TempDemoController {
+    @Resource
+    private ApplicationContext context;
+
     @GetMapping("/bean")
     public String registerBean() {
         //将applicationContext转换为ConfigurableApplicationContext
-        ConfigurableApplicationContext configurableApplicationContext = (ConfigurableApplicationContext) SpringContextUtil.getApplicationContext();
+        ConfigurableApplicationContext configurableApplicationContext = (ConfigurableApplicationContext) context;
 
         // 获取bean工厂并转换为DefaultListableBeanFactory
         DefaultListableBeanFactory defaultListableBeanFactory = (DefaultListableBeanFactory) configurableApplicationContext.getBeanFactory();
@@ -28,8 +34,8 @@ public class TempDemoController {
         // 注册bean
         defaultListableBeanFactory.registerBeanDefinition("userComponent", beanDefinitionBuilder.getRawBeanDefinition());
 
-
-        UserComponent userComponent = (UserComponent) SpringContextUtil.getBean("userComponent");
+        // 从spring 框架中取出刚注册的这个bean 对象
+        UserComponent userComponent = context.getBean("userComponent", UserComponent.class);
 
         return userComponent.toAction("动态注册生成调用");
 
