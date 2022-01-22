35f5c25c7ba67de2c1e1121070e2ba0c4bb28507
苏云弘 <su_787910081@sina.com>
2022-01-22 18:20:24 +0800

    nothing

--- demo-52-01-prometheus/src/main/java/com/suyh/demo/DemoApplication5201.java
+++ demo-52-01-prometheus/src/main/java/com/suyh/demo/DemoApplication5201.java
@@ -1,12 +1,11 @@
-
 package com.suyh.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication5201 {
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(DemoApplication5201.class, args);
     }
 }
