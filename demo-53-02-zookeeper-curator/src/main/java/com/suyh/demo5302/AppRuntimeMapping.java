9c69c15ee60f0a00bfb48706eeacae6095919d11
suyh <su_787910081@sina.com>
2022-02-15 10:57:17 +0800

    Create AppRuntimeMapping.java

---
+++ demo-53-02-zookeeper-curator/src/main/java/com/suyh/demo5302/AppRuntimeMapping.java
@@ -0,0 +1,109 @@
+
+package com.suyh.demo5302;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.recipes.cache.ChildData;
+import org.apache.curator.framework.recipes.cache.CuratorCache;
+import org.apache.curator.framework.recipes.cache.CuratorCacheListener;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+@Slf4j
+public class AppRuntimeMapping implements CuratorCacheListener, InitializingBean {
+    public static final String LISTENER_PATH = "/mapping/app-instance";
+    private final CuratorFramework curator;
+
+    // key: app name, value: arms runtime instance ip:port
+    private final Map<String, String> mapping = new ConcurrentHashMap<>();
+
+    public AppRuntimeMapping(CuratorFramework curator) {
+        this.curator = curator;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        start();
+    }
+
+    public void start() {
+        CuratorCache curatorCache = CuratorCache.build(curator, LISTENER_PATH);
+        // 注册监听器
+        curatorCache.listenable().addListener(this);
+        // 开启监听器
+        curatorCache.start();
+    }
+
+    @Override
+    public void event(Type type, ChildData oldData, ChildData data) {
+        switch (type) {
+            case NODE_CREATED: {
+                log.info("create event, oldData: {}, newData: {}", oldData, data);
+                if (data == null) {
+                    break;
+                }
+                String path = data.getPath();
+                if (LISTENER_PATH.equals(path)) {
+                    break;
+                }
+                String appName = path.substring(LISTENER_PATH.length() + 1);
+                if (appName.indexOf("/") > 0) {
+                    log.error("illegal resource name: {}", appName);
+                    break;
+                }
+                String runtimeInstance = new String(data.getData());
+                log.info("path: {}, application name: {}, data: {}", path, appName, runtimeInstance);
+                mapping.put(appName, runtimeInstance);
+            }
+            break;
+            case NODE_DELETED: {
+                log.info("delete event, oldData: {}, newData: {}", oldData, data);
+                if (oldData == null) {
+                    break;
+                }
+                String path = oldData.getPath();
+                if (LISTENER_PATH.equals(path)) {
+                    break;
+                }
+                String appName = path.substring(LISTENER_PATH.length() + 1);
+                if (appName.indexOf("/") > 0) {
+                    log.error("illegal application name: {}", appName);
+                    break;
+                }
+                mapping.remove(appName);
+            }
+            break;
+            case NODE_CHANGED: {
+                log.info("set_data, oldData: {}, newData: {}", oldData, data);
+                if (data == null) {
+                    break;
+                }
+                String path = data.getPath();
+                if (LISTENER_PATH.equals(path)) {
+                    break;
+                }
+                String appName = path.substring(LISTENER_PATH.length() + 1);
+                if (appName.indexOf("/") > 0) {
+                    log.error("illegal resource name: {}", appName);
+                    break;
+                }
+                String runtimeInstance = new String(data.getData());
+                log.info("path: {}, application name: {}, data: {}", path, appName, runtimeInstance);
+                mapping.put(appName, runtimeInstance);
+            }
+            break;
+            default: {
+                log.info("node default");
+            }
+            break;
+        }
+    }
+
+    public String get(String appName) {
+        return mapping.get(appName);
+    }
+}
