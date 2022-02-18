9c2a0c86051ee3fe90e52dae25a14ca779bc3550
苏云弘 <su_787910081@sina.com>
2022-02-19 00:52:25 +0800

    test

--- demo-22-validation-01/src/test/java/com/suyh/TestValidation.java
+++ demo-22-validation-01/src/test/java/com/suyh/TestValidation.java
@@ -4,19 +4,19 @@ import com.suyh.entity.ChildEntity;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.client.RestClientException;
 
 import javax.annotation.Resource;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(
         classes = {ValidationApplication.class},
         webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -31,19 +31,21 @@ public class TestValidation {
     public void test01() {
         String url = PRE_URL + "/get/child/entity";
         ChildEntity childEntity = new ChildEntity();
-        try {
-            ResponseEntity<ChildEntity> responseChildEntity
-                    = restTemplate.postForEntity(url, childEntity, ChildEntity.class);
-            log.info("responseChildEntity: {}", ToStringBuilder.reflectionToString(
-                    responseChildEntity, ToStringStyle.JSON_STYLE));
-            Assert.assertEquals(HttpStatus.BAD_REQUEST.value(), responseChildEntity.getStatusCodeValue());
-        } catch (RestClientException ex) {
-            log.error("exception", ex);
-            Assert.fail("failed");
-        } catch (Exception ex) {
-            log.error("other exception", ex);
-            Assert.fail("other exception");
-        }
+        Assertions.assertDoesNotThrow(() -> {
+            try {
+                ResponseEntity<ChildEntity> responseChildEntity
+                        = restTemplate.postForEntity(url, childEntity, ChildEntity.class);
+                log.info("responseChildEntity: {}", ToStringBuilder.reflectionToString(
+                        responseChildEntity, ToStringStyle.JSON_STYLE));
+                Assertions.assertEquals(HttpStatus.BAD_REQUEST.value(), responseChildEntity.getStatusCodeValue());
+            } catch (RestClientException ex) {
+                log.error("exception", ex);
+                throw ex;
+            } catch (Exception ex) {
+                log.error("other exception", ex);
+                throw ex;
+            }
+        });
     }
 
     @Test
@@ -57,7 +59,7 @@ public class TestValidation {
                 = restTemplate.postForEntity(url, childEntity, ChildEntity.class);
         log.info("responseChildEntity: {}", ToStringBuilder.reflectionToString(
                 responseChildEntity, ToStringStyle.JSON_STYLE));
-        Assert.assertEquals(HttpStatus.BAD_REQUEST.value(), responseChildEntity.getStatusCodeValue());
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST.value(), responseChildEntity.getStatusCodeValue());
 
         // 大于 9999异常
         ChildEntity childEntity02 = new ChildEntity();
@@ -67,7 +69,7 @@ public class TestValidation {
                 = restTemplate.postForEntity(url, childEntity02, ChildEntity.class);
         log.info("responseChildEntity: {}", ToStringBuilder.reflectionToString(
                 responseChildEntity02, ToStringStyle.JSON_STYLE));
-        Assert.assertEquals(HttpStatus.BAD_REQUEST.value(), responseChildEntity02.getStatusCodeValue());
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST.value(), responseChildEntity02.getStatusCodeValue());
 
         // 1000 9999 正常范围，以及所有的值都是正常范围
         ChildEntity childEntity03 = new ChildEntity();
@@ -79,8 +81,8 @@ public class TestValidation {
                 = restTemplate.postForEntity(url, childEntity03, ChildEntity.class);
         log.info("responseChildEntity: {}", ToStringBuilder.reflectionToString(
                 responseChildEntity03, ToStringStyle.JSON_STYLE));
-        Assert.assertNotEquals(HttpStatus.BAD_REQUEST.value(), responseChildEntity03.getStatusCodeValue());
-        Assert.assertEquals(HttpStatus.OK.value(), responseChildEntity03.getStatusCodeValue());
+        Assertions.assertNotEquals(HttpStatus.BAD_REQUEST.value(), responseChildEntity03.getStatusCodeValue());
+        Assertions.assertEquals(HttpStatus.OK.value(), responseChildEntity03.getStatusCodeValue());
     }
 
     @Test
@@ -90,6 +92,6 @@ public class TestValidation {
                 url, String.class);
         String jsonResp = ToStringBuilder.reflectionToString(resp, ToStringStyle.JSON_STYLE);
         log.info("jsonResp: {}", jsonResp);
-        Assert.assertEquals(HttpStatus.BAD_REQUEST.value(), resp.getStatusCodeValue());
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST.value(), resp.getStatusCodeValue());
     }
 }
