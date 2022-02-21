1d46a64df1fbacbf9685acde6dbdae5636383655
suyh <su_787910081@sina.com>
2022-02-21 09:08:20 +0800

    Update Demo0403Configuration.java

--- demo-04-scheduler-03-shedlock-custom/src/main/java/com/suyh0403/config/Demo0403Configuration.java
+++ demo-04-scheduler-03-shedlock-custom/src/main/java/com/suyh0403/config/Demo0403Configuration.java
@@ -14,11 +14,14 @@ public class Demo0403Configuration {
     @ConditionalOnMissingBean(LockProvider.class)
     @Bean
     public LockProvider lockProvider3(DataSource dataSource) {
+        JdbcTemplateLockProvider.ColumnNames columnNames = new JdbcTemplateLockProvider.ColumnNames(
+                "name", "lock_until", "locked_at", "locked_by");
         JdbcTemplateLockProvider.Configuration shdlck = JdbcTemplateLockProvider.Configuration.builder()
                 .withTableName("shedlock")
                 .withJdbcTemplate(new JdbcTemplate(dataSource))
                 .usingDbTime() // Works on Postgres, MySQL, MariaDb, MS SQL, Oracle, DB2, HSQL and H2
                 .withLockedByValue("suyh")  // 表中locked_by 字段的值，默认是当前主机的主机名。
+                .withColumnNames(columnNames)
                 .build();
         return new JdbcTemplateLockProvider(shdlck);
     }
