f1d66a2cd76c2e6c92b90348e0d2b4e22a784593
苏云弘 <su_787910081@sina.com>
2022-03-11 22:39:36 +0800

    nothing

---
+++ demo-53-03-zookeeper-listener/src/main/java/com/suyh5303/Application5303.java
@@ -0,0 +1,11 @@
+package com.suyh5303;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application5303 {
+    public static void main(String[] args) {
+        SpringApplication.run(Application5303.class, args);
+    }
+}
