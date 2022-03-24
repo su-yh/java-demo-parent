01142df9e937d1cf2d18a2096ec401a174f0065e
suyh <su_787910081@sina.com>
2022-03-24 10:16:17 +0800

    Update FilterExceptionController.java

--- demo-21-filter-exception-02/src/main/java/com/suyh/demo2102/handler/controller/FilterExceptionController.java
+++ demo-21-filter-exception-02/src/main/java/com/suyh/demo2102/handler/controller/FilterExceptionController.java
@@ -1,15 +1,19 @@
 package com.suyh.demo2102.handler.controller;
 
-import org.springframework.boot.autoconfigure.web.ErrorProperties;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
 import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -17,12 +21,14 @@ import java.util.Map;
  * 刚测试了一下，当该controller 存在，且@RestControllerAdvice 实现的类也存在，则filter 里面的异常将会走此异常处理类
  * 而mvc 的异常，将会按正常的@RestControllerAdvice 的实现类处理。
  * 如果没有实现@RestControllerAdvice 的实现类，则异常全部都会走这里。
+ *
+ * 另外参考：https://blog.csdn.net/L_Sail/article/details/70198886
  */
 @Controller
 public class FilterExceptionController extends BasicErrorController {
 
-    public FilterExceptionController() {
-        super(new DefaultErrorAttributes(), new ErrorProperties());
+    public FilterExceptionController(ServerProperties serverProperties) {
+        super(new DefaultErrorAttributes(), serverProperties.getError());
     }
 
     @Override
@@ -38,9 +44,25 @@ public class FilterExceptionController extends BasicErrorController {
          * "message": "suyh exception message",
          * "path": "/filter/req/runtime/exception"
          * }
+         * Map<String, Object> body = getErrorAttributes(request, isIncludeStackTrace(request, MediaType.ALL));
          */
-        Map<String, Object> body = getErrorAttributes(request, isIncludeStackTrace(request, MediaType.ALL));
+        Map<String, Object> body = new HashMap<>();
         body.put("status", HttpStatus.OK.value());
-        return new ResponseEntity<>(body, HttpStatus.OK);
+        HttpHeaders headers  = new HttpHeaders();
+        headers.set(HttpHeaders.CONTENT_TYPE, "application/json");
+        ResponseEntity<Map<String, Object>> responseEntity = new ResponseEntity<>(body, headers, HttpStatus.OK);
+        return responseEntity;
+    }
+
+    /**
+     * 在request 的请求头中有指定一个 "Accept: text/html" 时，会走该方法。
+     */
+    @Override
+    public ModelAndView errorHtml(HttpServletRequest request, HttpServletResponse response) {
+        HttpStatus status = getStatus(request);
+        response.setStatus(status.value());
+        final Map<String, Object> model = getErrorAttributes(request, isIncludeStackTrace(request, MediaType.TEXT_HTML));
+        final ModelAndView modelAndView = resolveErrorView(request, response, status, model);
+        return (modelAndView == null ? new ModelAndView("error", model) : modelAndView);
     }
 }
