f1c4c0365b9cb8e60fe785265bf22fa25069fec8
苏云弘 <su_787910081@sina.com>
2022-05-08 17:44:45 +0800

    websocket 拦截器

---
+++ demo-55-04-websocket/src/main/java/com/suyh5504/websocket/server/interceptor/SocketChannelInterceptor.java
@@ -0,0 +1,74 @@
+package com.suyh5504.websocket.server.interceptor;
+
+import org.springframework.lang.NonNull;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+
+/**
+ *  另外 一种拦截器
+ */
+public class SocketChannelInterceptor implements ChannelInterceptor {
+
+    /**
+     * 在完成发送之后进行调用，不管是否有异常发生，一般用于资源清理
+     */
+    @Override
+    public void afterSendCompletion(@NonNull Message<?> message, @NonNull MessageChannel channel,
+                                    boolean sent, Exception ex) {
+        System.out.println("SocketChannelIntecepter->afterSendCompletion");
+    }
+
+
+    /**
+     * 在消息被实际发送到频道之前调用
+     */
+    @Override
+    public Message<?> preSend(@NonNull Message<?> message, @NonNull MessageChannel channel) {
+        System.out.println("SocketChannelIntecepter->preSend");
+        return message;
+    }
+
+    /**
+     * 发送消息调用后立即调用
+     */
+    @Override
+    public void postSend(@NonNull Message<?> message, @NonNull MessageChannel channel,
+                         boolean sent) {
+        System.out.println("SocketChannelIntecepter->postSend");
+
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(message);//消息头访问器
+
+        if (headerAccessor.getCommand() == null ) return ;// 避免非stomp消息类型，例如心跳检测
+
+        String sessionId = headerAccessor.getSessionAttributes().get("sessionId").toString();
+        System.out.println("SocketChannelIntecepter -> sessionId = "+sessionId);
+
+        switch (headerAccessor.getCommand()) {
+            case CONNECT:
+                connect(sessionId);
+                break;
+            case DISCONNECT:
+                disconnect(sessionId);
+                break;
+            case SUBSCRIBE:
+                break;
+            case UNSUBSCRIBE:
+                break;
+            default:
+                break;
+        }
+    }
+
+    //连接成功
+    private void connect(String sessionId){
+        System.out.println("connect sessionId="+sessionId);
+    }
+
+    //断开连接
+    private void disconnect(String sessionId){
+        System.out.println("disconnect sessionId="+sessionId);
+        //用户下线操作
+    }
+}
