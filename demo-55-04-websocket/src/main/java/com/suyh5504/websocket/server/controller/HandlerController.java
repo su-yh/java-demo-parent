c6d4145e558f7cf4a66e940c75f3568e566b3837
苏云弘 <su_787910081@sina.com>
2022-05-08 21:18:47 +0800

    websocket 拦截器

--- demo-55-04-websocket/src/main/java/com/suyh5504/websocket/server/controller/HandlerController.java
+++ demo-55-04-websocket/src/main/java/com/suyh5504/websocket/server/controller/HandlerController.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 
@@ -22,7 +23,7 @@ public class HandlerController {
 
     @MessageMapping("/chat")
     @SendTo("/topic/messages")
-    public OutputMessage send(Message message) throws Exception {
+    public OutputMessage send(Message message, StompHeaderAccessor headerAccessor) throws Exception {
         log.info("message: {}", message);
         String time = new SimpleDateFormat("HH:mm").format(new Date());
         return new OutputMessage(message.getFrom(), message.getText(), time);
