018acf8dadeadc3a046d008c9b6292e444638f92
suyh <su_787910081@sina.com>
2022-08-09 16:54:27 +0800

    Create Application0302.java

---
+++ demo-03-jedis-02-lettuce/src/main/java/com/suyh0302/Application0302.java
@@ -0,0 +1,11 @@
+package com.suyh0302;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application0302 {
+    public static void main(String[] args) {
+        SpringApplication.run(Application0302.class, args);
+    }
+}
