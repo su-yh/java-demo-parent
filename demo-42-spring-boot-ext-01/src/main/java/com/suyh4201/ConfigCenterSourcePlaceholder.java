9a765253eb564ec0bcb3f943efad11a23d514a18
suyh <su_787910081@sina.com>
2022-08-27 20:47:46 +0800

    Update and rename ConfigCenterLoader.java to ConfigCenterSourcePlaceholder.java

--- demo-42-spring-boot-ext-01/src/main/java/com/suyh4201/ConfigCenterSourcePlaceholder.java
+++ demo-42-spring-boot-ext-01/src/main/java/com/suyh4201/ConfigCenterSourcePlaceholder.java
@@ -19,32 +19,24 @@ import java.util.Map;
  *
  * @since 2021-06-18
  */
-public class ConfigCenterLoader implements EnvironmentPostProcessor, Ordered {
+public class ConfigCenterSourcePlaceholder implements EnvironmentPostProcessor, Ordered {
 
     /**
      * 配置中心属性名，用于存放在系统属性中。
      */
-    public static final String CONFIG_CENTER_NAME = "configCenter";
-
-    // 此属性要生效必须放在命令行参数上
-    // 当然，放到系统环境变量中也是可以的。
-    public static final String ENABLED_CONFIG_CENTER = "suyh.ext.config.center.enabled";
+    public static final String SOURCE_PLACEHOLDER = "sourcePlaceholder";
 
     @Override
     public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
-        // 是否禁止连接到配置中心
-        Boolean enabledConfigCenter = environment.getProperty(ENABLED_CONFIG_CENTER, Boolean.class, Boolean.TRUE);
-        if (!enabledConfigCenter) {
-            return;
-        }
-
         Map<String, Object> configCenterProperties = buildMap();
         MutablePropertySources sources = environment.getPropertySources();
-        if (sources.contains(CONFIG_CENTER_NAME)) {
-            throw new RuntimeException("duplicate config property source name " + CONFIG_CENTER_NAME);
+        if (sources.contains(SOURCE_PLACEHOLDER)) {
+            throw new RuntimeException("duplicate config property source name " + SOURCE_PLACEHOLDER);
         }
-        // 这里是添加到后面，现在前面应该是有系统变量，环境变量，命令行参数。
-        sources.addLast(new MapPropertySource(CONFIG_CENTER_NAME, configCenterProperties));
+        // 现在前面已经加载了系统变量，环境变量，命令行参数的相关配置属性到环境中了。
+        // 为配置中心的配置属性资源占位。后面会从配置中心拉取相关数据然后替换该位置上的所有配置属性。
+        // 即： 这里的configCenterProperties 是没有任何意义的。
+        sources.addLast(new MapPropertySource(SOURCE_PLACEHOLDER, configCenterProperties));
     }
 
     private Map<String, Object> buildMap() {
