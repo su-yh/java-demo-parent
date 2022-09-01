c43084848475c94ad74c363d4189bd6c2864581d
suyh <su_787910081@sina.com>
2022-09-01 17:32:03 +0800

    Update ConfigCenterEnvironmentPostProcessor.java

--- demo-02-config-center-01/src/main/java/com/suyh0201/environment/ConfigCenterEnvironmentPostProcessor.java
+++ demo-02-config-center-01/src/main/java/com/suyh0201/environment/ConfigCenterEnvironmentPostProcessor.java
@@ -2,19 +2,21 @@ package com.suyh0201.environment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.context.config.ConfigFileApplicationListener;
+import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
 import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.boot.logging.DeferredLog;
-import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.core.Ordered;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.MapPropertySource;
 import org.springframework.core.env.MutablePropertySources;
 import org.springframework.core.env.PropertySource;
+import org.springframework.lang.NonNull;
 
 import java.util.HashMap;
 
-public class ConfigCenterEnvironmentPostProcessor implements EnvironmentPostProcessor, ApplicationListener<ApplicationEvent>, Ordered {
+public class ConfigCenterEnvironmentPostProcessor
+        implements EnvironmentPostProcessor, ApplicationListener<ApplicationEnvironmentPreparedEvent>, Ordered {
 
     /**
      * 由于此时的日志系统还没有初始化，这里使用spring boot 提供的缓存日志。
@@ -29,7 +31,14 @@ public class ConfigCenterEnvironmentPostProcessor implements EnvironmentPostProc
     }
 
     @Override
-    public void onApplicationEvent(ApplicationEvent event) {
+    public void onApplicationEvent(@NonNull ApplicationEnvironmentPreparedEvent event) {
+        /*
+         * 如下的日志代码要生效，则需要它的执行时机在 LoggingApplicationListener.onApplicationEnvironmentPreparedEvent 之后
+         * 这里由于order 的值是(Ordered.HIGHEST_PRECEDENCE + 10) 而LoggingApplicationListener.DEFAULT_ORDER是(Ordered.HIGHEST_PRECEDENCE + 20)
+         * 使得当前的优先级高于LoggingApplicationListener，所以如果在这里添加则当前日志系统中的相关日志将不会被正常打印出来。
+         * 所以这个LOGGER 的日志要通过其他方式处理，或者监听的事件改一个在ApplicationEnvironmentPreparedEvent 之后的，又或者另外实现一个监听器
+         * 在那个监听器里面实现这个LOGGER。
+         */
         // 在事件监听发生时，将日志写到日志文件中。
         LOGGER.replayTo(ConfigCenterEnvironmentPostProcessor.class);
     }
