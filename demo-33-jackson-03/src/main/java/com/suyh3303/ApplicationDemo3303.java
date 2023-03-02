c46966fd1527ee530fac18b5838ced937c8e89b1
苏云弘 <su_787910081@sina.com>
2023-03-02 22:45:26 +0800

    1111

---
+++ demo-33-jackson-03/src/main/java/com/suyh3303/ApplicationDemo3303.java
@@ -0,0 +1,11 @@
+package com.suyh3303;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationDemo3303 {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationDemo3303.class, args);
+    }
+}
--- demo-33-jackson-03/src/main/java/com/suyh3303/controller/SuyhDemoJacksonBuilderCustomizer.java
+++ demo-33-jackson-03/src/main/java/com/suyh3303/controller/SuyhDemoJacksonBuilderCustomizer.java
@@ -1,13 +1,16 @@
-import lombok.extern.slf4j.Slf4j;
+package com.suyh3303.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
 
 /**
  * 自定义spring web 中对jackson 的定义处理
  */
 @Component
-@Slf4j
 public class SuyhDemoJacksonBuilderCustomizer implements Jackson2ObjectMapperBuilderCustomizer {
     @Resource(type = ObjectMapper.class)
     private ObjectMapper objectMapper;
