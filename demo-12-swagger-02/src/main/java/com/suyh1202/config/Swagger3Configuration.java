ff237fd7942d7509ea22885b96e070ce22f48bea
苏云弘 <su_787910081@sina.com>
2023-03-22 22:10:21 +0800

    1111

--- demo-12-swagger-02/src/main/java/com/suyh1202/config/Swagger3Configuration.java
+++ demo-12-swagger-02/src/main/java/com/suyh1202/config/Swagger3Configuration.java
@@ -33,7 +33,6 @@ import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
 import java.util.Arrays;
