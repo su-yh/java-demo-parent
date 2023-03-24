ebbb98fbfe394449387213cbcb288febcfca9628
苏云弘 <su_787910081@sina.com>
2023-03-24 23:30:01 +0800

    1111

--- demo-03-redis-04-multi-datasource/src/main/java/com/suyh0304/config/SuyhRedisConfiguration.java
+++ demo-03-redis-04-multi-datasource/src/main/java/com/suyh0304/config/SuyhRedisConfiguration.java
@@ -4,6 +4,9 @@ import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 
 @Configuration
 public class SuyhRedisConfiguration {
@@ -17,5 +20,31 @@ public class SuyhRedisConfiguration {
     @Bean(name = "otherRedisProperties")
     public RedisProperties suyhRedisProperties() {
         return new RedisProperties();
+    }
+
+    protected static RedisClusterConfiguration getClusterConfiguration(RedisProperties properties) {
+        if (properties.getCluster() == null) {
+            return null;
+        }
+        RedisProperties.Cluster clusterProperties = properties.getCluster();
+        RedisClusterConfiguration config = new RedisClusterConfiguration(clusterProperties.getNodes());
+        if (clusterProperties.getMaxRedirects() != null) {
+            config.setMaxRedirects(clusterProperties.getMaxRedirects());
+        }
+        config.setUsername(properties.getUsername());
+        if (properties.getPassword() != null) {
+            config.setPassword(RedisPassword.of(properties.getPassword()));
+        }
+        return config;
+    }
+
+    protected static RedisStandaloneConfiguration getStandaloneConfig(RedisProperties properties) {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(properties.getHost());
+        config.setPort(properties.getPort());
+        config.setUsername(properties.getUsername());
+        config.setPassword(RedisPassword.of(properties.getPassword()));
+        config.setDatabase(properties.getDatabase());
+        return config;
     }
 }
