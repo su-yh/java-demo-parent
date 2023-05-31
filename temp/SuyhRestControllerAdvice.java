aba979ced82cf9e9d79f8a103c8c0b9061575c41
suyh <su_787910081@sina.com>
2023-05-31 09:22:44 +0800

    Update SuyhRestControllerAdvice.java

--- temp/SuyhRestControllerAdvice.java
+++ temp/SuyhRestControllerAdvice.java
@@ -6,8 +6,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 /**
  * 自定义异常的处理方式
  * 其他的异常处理放到默认处理异常里面。
+ * 
+ * 可能这个真的不适合在这里使用，不要它了吧。
+ * 使用自定义的 HandlerExceptionResolver 来处理自己的异常，其他的异常还是交给spring mvc 为我们提供的默认处理器。
  */
-@RestControllerAdvice
+// @RestControllerAdvice
 @Slf4j
 public class SuyhRestControllerAdvice {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
