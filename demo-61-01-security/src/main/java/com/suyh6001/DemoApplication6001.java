16b8bedd92bc66a6a20e34da9ebbed87327c2274
suyh <su787910081@163.com>
2023-11-02 19:04:56 +0800

    security

---
+++ demo-61-01-security/src/main/java/com/suyh6001/DemoApplication6001.java
@@ -0,0 +1,15 @@
+package com.suyh6001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author suyh
+ * @since 2023-11-02
+ */
+@SpringBootApplication
+public class DemoApplication6001 {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication6001.class, args);
+    }
+}
