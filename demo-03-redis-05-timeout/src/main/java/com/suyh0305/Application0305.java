2821b3906e7e45ee6b4669951d30dbc8e3492d6b
suyh <su787910081@163.com>
2023-11-08 15:33:34 +0800

    nothing

---
+++ demo-03-redis-05-timeout/src/main/java/com/suyh0305/Application0305.java
@@ -0,0 +1,11 @@
+package com.suyh0305;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application0305 {
+    public static void main(String[] args) {
+        SpringApplication.run(Application0305.class, args);
+    }
+}
---
+++ demo-03-redis-05-timeout/src/main/java/com/suyh0305/init/SuyhTestInit.java
@@ -0,0 +1,15 @@
+package com.suyh0305.init;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class SuyhTestInit implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) {
+    }
+}
