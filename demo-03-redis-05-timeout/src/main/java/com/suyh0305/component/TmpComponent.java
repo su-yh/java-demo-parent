85e442675b66b07f3a1bca628fdbe5e475c20afd
suyh <su787910081@163.com>
2023-11-10 18:06:56 +0800

    全局缓存默认有效时间

--- demo-03-redis-05-timeout/src/main/java/com/suyh0305/component/TmpComponent.java
+++ demo-03-redis-05-timeout/src/main/java/com/suyh0305/component/TmpComponent.java
@@ -13,6 +13,8 @@ import java.util.Date;
 @Component
 public class TmpComponent {
 
+    // 在支持Spring Cache的环境下，对于使用@Cacheable标注的方法，Spring在每次执行前都会检查Cache中是否存在相同key的缓存元素，
+    // 如果存在就不再执行该方法，而是直接从缓存中获取结果进行返回，否则才会执行并将返回结果存入指定的缓存中。
     @Cacheable(cacheNames = "suyh-test-time", key = "#key01")
     public String getValue(String key01) {
         System.out.println("getValue 被调用");
