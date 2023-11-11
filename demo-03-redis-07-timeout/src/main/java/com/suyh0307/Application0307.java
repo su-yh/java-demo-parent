b290a898c2758d19309f273012667f88c2bf6def
suyh <su787910081@163.com>
2023-11-11 09:27:09 +0800

    全局缓存默认有效时间

---
+++ demo-03-redis-07-timeout/src/main/java/com/suyh0307/Application0307.java
@@ -0,0 +1,11 @@
+package com.suyh0307;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application0307 {
+    public static void main(String[] args) {
+        SpringApplication.run(Application0307.class, args);
+    }
+}
