f100b869a7f98fbb2fe7fb5e3e1b28bfe1ad1cf6
suyh <suyunhong@ater-info.com>
2023-11-20 17:38:15 +0800

    nothing

--- demo-03-redis-10-timeout_simple/src/main/java/com/suyh0310/config/RedisAutoConfiguration.java
+++ demo-03-redis-10-timeout_simple/src/main/java/com/suyh0310/config/RedisAutoConfiguration.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 import java.time.Duration;
 
@@ -28,6 +30,11 @@ public class RedisAutoConfiguration {
         redisCacheConfiguration = redisCacheConfiguration.entryTtl(Duration.ofSeconds(30)).prefixCacheNameWith("suyh-prefix:")
         // 参考：org.springframework.data.redis.cache.CacheKeyPrefix.prefixed()
         //         .computePrefixWith(name -> "suyh-prefix" + name + SEPARATOR)    // 这里可以处理那个尾巴上的双冒号的问题，但是这样处理似乎并不好，还是就按他默认的方式处理就好了，只是连续两个冒号而以没关系。
+        //         .serializeValuesWith(null) // 在这里可以指定value 的序列化逻辑   默认使用的是：RedisSerializerToSerializationPairAdapter
+                // TODO: suyh - 所以这里如果需要自定义序列化与反序列化的逻辑的话，只需要模仿StringRedisSerializer 的实现即可。
+                // .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                // 这里直接使用jackson 的序列化方式就好了，似乎可以直接使用bean 对象呀，但是具体还不清楚。
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new Jackson2JsonRedisSerializer(Object.class)))
         ;
 
         return new SuyhRedisCacheManager(
