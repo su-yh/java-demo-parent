5752572efe225a497df2f678b1c311139c290c42
suyh <suyunhong@ater-info.com>
2023-11-23 19:17:06 +0800

    优雅停机是调用close() 方法，而非stop() 方法

--- demo-03-redis-10-timeout_simple/src/main/java/com/suyh0310/Application0310.java
+++ demo-03-redis-10-timeout_simple/src/main/java/com/suyh0310/Application0310.java
@@ -2,13 +2,10 @@ package com.suyh0310;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application0310 {
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(Application0310.class, args);
-
-        System.out.println("结束了");
+        SpringApplication.run(Application0310.class, args);
     }
 }
