fb07d1ccfd9a7f968b4f11de8a89999e44805d45
suyh <suyunhong@ater-info.com>
2023-11-29 15:03:15 +0800

    rabbitmq

---
+++ demo-07-rabbitmq-02/src/main/java/com/suyh07012/component/TopicWomenReceiverComponent.java
@@ -0,0 +1,25 @@
+package com.suyh07012.component;
+
+import com.suyh07011.config.TopicRabbitConfig;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * @author suyh
+ * @since 2023-11-29
+ */
+@Component
+@RequiredArgsConstructor
+@RabbitListener(queues = TopicRabbitConfig.TOPIC_WOMEN)
+@Slf4j
+public class TopicWomenReceiverComponent {
+    @RabbitHandler
+    public void process(Map<String, Object> testMessage) {
+        System.out.println("TopicWomenReceiverComponent 消费者收到消息：" + testMessage.toString());
+    }
+}
