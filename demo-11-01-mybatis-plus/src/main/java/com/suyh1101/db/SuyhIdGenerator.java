57e4170c25f164132fc4ef72cbae8682d9e61a1c
suyh <suyunhong@ater-info.com>
2023-12-09 10:24:17 +0800

    id 生成器

--- demo-11-01-mybatis-plus/src/main/java/com/suyh1101/db/SuyhIdGenerator.java
+++ demo-11-01-mybatis-plus/src/main/java/com/suyh1101/db/SuyhIdGenerator.java
@@ -2,11 +2,13 @@ package com.suyh1101.db;
 
 import com.baomidou.mybatisplus.core.incrementer.IdentifierGenerator;
 import com.baomidou.mybatisplus.core.toolkit.Sequence;
+import org.springframework.stereotype.Component;
 
 /**
  * @author suyh
  * @since 2023-12-09
  */
+@Component
 public class SuyhIdGenerator implements IdentifierGenerator {
     private final Sequence sequence;
 
@@ -17,5 +19,10 @@ public class SuyhIdGenerator implements IdentifierGenerator {
     @Override
     public Number nextId(Object entity) {
         return sequence.nextId();
+    }
+
+    @Override
+    public String nextUUID(Object entity) {
+        return IdentifierGenerator.super.nextUUID(entity);
     }
 }
--- demo-11-01-mybatis-plus/src/main/java/com/suyh1101/entity/TestUuidEntity.java
+++ demo-11-01-mybatis-plus/src/main/java/com/suyh1101/entity/TestUuidEntity.java
@@ -12,7 +12,8 @@ import lombok.Data;
 @TableName(value = "test_uuid", autoResultMap = true)
 @Data
 public class TestUuidEntity {
-    // 对于字符串的uuid，最终调用的是 IdentifierGenerator.nextUUID(..)
+    // 对于字符串的uuid，如果设置为: IdType.ASSIGN_UUID, 调用的是 IdentifierGenerator.nextUUID(..)
+    // 如果没有设置类型，则会直接生成数字类型的ID，然后添加到数据为中。
     @TableId(type = IdType.ASSIGN_UUID)
     private String id;
 
