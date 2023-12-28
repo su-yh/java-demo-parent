c603f8af9d6da4cf0871e5974a6b167b68fdc652
suyh <suyunhong@ater-info.com>
2023-12-28 18:45:15 +0800

    测试数据

--- demo-63-01-temp/src/main/java/com/suyh5802/web/base/runner/mq/util/MqCorrelationIdUtils.java
+++ demo-63-01-temp/src/main/java/com/suyh5802/web/base/runner/mq/util/MqCorrelationIdUtils.java
@@ -1,20 +1,13 @@
 package com.suyh5802.web.base.runner.mq.util;
 
-import java.util.concurrent.atomic.AtomicLong;
+import com.baomidou.mybatisplus.core.incrementer.DefaultIdentifierGenerator;
 
 /**
  * @author suyh
  * @since 2023-12-28
  */
 public class MqCorrelationIdUtils {
-    private static final AtomicLong correlationId = new AtomicLong();
-
-    static {
-        long nanoTime = System.nanoTime();
-        correlationId.set(nanoTime);
-    }
-
     public static long getCorrelationId() {
-        return correlationId.getAndIncrement();
+        return DefaultIdentifierGenerator.getInstance().nextId(null);
     }
 }
--- demo-63-01-temp/src/main/java/com/suyh5802/web/base/service/AdjustUserService.java
+++ demo-63-01-temp/src/main/java/com/suyh5802/web/base/service/AdjustUserService.java
@@ -1,9 +1,16 @@
 package com.suyh5802.web.base.service;
 
+import com.baomidou.mybatisplus.core.incrementer.DefaultIdentifierGenerator;
+import com.suyh5802.web.base.entity.AdjustUserEntity;
 import com.suyh5802.web.base.mapper.AdjustUserMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.time.LocalDate;
+import java.util.Random;
+import java.util.UUID;
 
 /**
  * @author suyh
@@ -15,33 +22,55 @@ import org.springframework.stereotype.Service;
 public class AdjustUserService {
     private final AdjustUserMapper adjustUserMapper;
 
-    // @PostConstruct
+    @PostConstruct
     public void init() {
-//        List<AdjustUserEntity> entities = adjustUserMapper.selectList(null);
-//        System.out.println("entities: " + entities);
-//
-//        adjustUserMapper.delete(null);
-//
-//        String uuid = UUID.randomUUID().toString().replace("-", "");
-//        AdjustUserEntity entity = new AdjustUserEntity();
-//        entity.setGaid(uuid).setPkg(uuid).setChannelid(uuid).setAppid(uuid)
-//                        .setAppNameDashboard(uuid).setAppToken(uuid).setTracker(uuid)
-//                        .setCampaignName(uuid).setAdgroupName(uuid).setCreativeName(uuid)
-//                        .setClickReferer(uuid).setInstalledAt(uuid).setDeviceName(uuid)
-//                        .setOsVersion(uuid).setTimezone(uuid).setEventName(uuid)
-//                        .setDeeplink(uuid).setCostAmount(uuid)
-//                        .setGoogleAdsCampaignType(uuid).setGoogleAdsCampaignName(uuid).setGoogleAdsCampaignId(uuid)
-//                        .setGoogleAdsAdgroupId(uuid).setGoogleAdsAdgroupName(uuid)
-//                        .setGoogleAdsCreativeId(uuid).setGoogleAdsKeyword(uuid).setGoogleAdsPlacement(uuid)
-//                        .setFbCampaignGroupId(uuid).setFbCampaignGroupName(uuid)
-//                        .setFbCampaignId(uuid).setFbCampaignName(uuid)
-//                        .setFbAdgroupName(uuid).setFbAdgroupId(uuid).setFbAdObjectiveName(uuid)
-//                        .setAdid(uuid).setIsOrganic("1").setIpAddress(uuid)
-//                        .setCity(uuid).setIsp(uuid).setLanguage(uuid)
-//                        .setKey(uuid).setCts(1L).setDates(2).setWebUuid(uuid);
-//
-//
-//
-//        adjustUserMapper.insert(entity);
+        // 是否启用下面的代码功能
+        boolean enabled = false;
+        if (!enabled) {
+            return;
+        }
+
+        long currentTimeMillis = System.currentTimeMillis();
+        long timestampSecond = currentTimeMillis / 1000;
+        String[] pkgs = {"com.sn.oos", "com.rruo.goldennludo.ogott"};
+
+        Random random = new Random();
+
+        for (int i = 0; i < 1000; i++) {
+            int channelNumber = random.nextInt(9000000) + 1000000;
+            String channelid = "slg_" + channelNumber;
+            int pkgIndex = channelNumber % 2;
+            String pkg = pkgs[pkgIndex];
+            long googleAdsCampaignId = DefaultIdentifierGenerator.getInstance().nextId(null);
+            long googleAdsAdgroupId = DefaultIdentifierGenerator.getInstance().nextId(null);
+            long googleAdsCreativeId = DefaultIdentifierGenerator.getInstance().nextId(null);
+            long fbCampaignId = DefaultIdentifierGenerator.getInstance().nextId(null);
+            long fbAdgroupId = DefaultIdentifierGenerator.getInstance().nextId(null);
+
+            LocalDate localDate = LocalDate.now();
+            String dateString = localDate.toString().replace("-", "");
+            int dateInt = Integer.parseInt(dateString);
+
+            String uuid = UUID.randomUUID().toString().replace("-", "");
+            AdjustUserEntity entity = new AdjustUserEntity();
+            entity.setGaid(uuid).setPkg(pkg).setChannelid(channelid).setAppid(pkg)
+                    .setAppNameDashboard(pkg).setAppToken("111111").setTracker("tracker")
+                    .setCampaignName(uuid).setAdgroupName(uuid).setCreativeName(uuid)
+                    .setClickReferer(uuid).setInstalledAt("" + timestampSecond).setDeviceName("Y53s")
+                    .setOsVersion("13").setTimezone("UTC+0530").setEventName("")
+                    .setDeeplink("").setCostAmount("")
+                    .setGoogleAdsCampaignType("ACI").setGoogleAdsCampaignName(googleAdsCampaignId + "").setGoogleAdsCampaignId(googleAdsCampaignId + "")
+                    .setGoogleAdsAdgroupId(googleAdsAdgroupId + "").setGoogleAdsAdgroupName(googleAdsAdgroupId + "")
+                    .setGoogleAdsCreativeId(googleAdsCreativeId + "").setGoogleAdsKeyword(uuid).setGoogleAdsPlacement(uuid)
+                    .setFbCampaignGroupId(uuid).setFbCampaignGroupName(uuid)
+                    .setFbCampaignId(fbCampaignId + "").setFbCampaignName(fbCampaignId + "")
+                    .setFbAdgroupName(fbAdgroupId + "").setFbAdgroupId(fbAdgroupId + "").setFbAdObjectiveName(uuid)
+                    .setAdid(uuid).setIsOrganic("0").setIpAddress(uuid)
+                    .setCity("").setIsp("").setLanguage("en")
+                    .setKey(uuid).setCts(timestampSecond).setDates(dateInt).setWebUuid(uuid);
+
+            adjustUserMapper.insert(entity);
+        }
+
     }
 }
