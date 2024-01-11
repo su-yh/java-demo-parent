d47b1c73b1a459d7e48b4063fc7589d69592997d
suyh <suyunhong@ater-info.com>
2024-01-11 17:17:26 +0800

    11

---
+++ demo-64-springboot_properties_yaml/demo64-01-load/src/main/java/com/suyh/d64/LoadPropertiesFromAbsolutePath.java
@@ -0,0 +1,35 @@
+package com.suyh.d64;
+
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.EncodedResource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
+
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * @author suyh
+ * @since 2024-01-11
+ */
+public class LoadPropertiesFromAbsolutePath {
+
+    public static void main(String[] args) {
+        String absoluteFilePath = "/opt/trend_oper/flink-job-suyh.properties";
+        Properties properties = loadProperties(absoluteFilePath);
+
+        // Access properties
+        String propertyValue = properties.getProperty("rmqPrefetchCount");
+        System.out.println("rmqPrefetchCount = " + propertyValue);
+    }
+
+    private static Properties loadProperties(String absoluteFilePath) {
+        try {
+            Resource resource = new DefaultResourceLoader().getResource("file:" + absoluteFilePath);
+            EncodedResource encodedResource = new EncodedResource(resource);
+            return PropertiesLoaderUtils.loadProperties(encodedResource);
+        } catch (IOException e) {
+            throw new RuntimeException("Error loading properties", e);
+        }
+    }
+}
