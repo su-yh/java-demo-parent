ba7aeee343dec416b84a1597f3b9a20555d435bf
suyh <suyunhong@ater-info.com>
2024-01-11 17:37:59 +0800

    11

--- demo-64-springboot_properties_yaml/demo64-01-load/src/main/java/com/suyh/d64/yaml/YamlResourceLoad.java
+++ demo-64-springboot_properties_yaml/demo64-01-load/src/main/java/com/suyh/d64/yaml/YamlResourceLoad.java
@@ -13,29 +13,28 @@ import java.util.Properties;
  */
 public class YamlResourceLoad {
     public static void main(String[] args) {
-        // 从类路径加载 YAML 文件
-        String classpathYamlPath = "classpath:/suyh_properties.properties";
-        Properties classpathProperties = loadYamlProperties(classpathYamlPath);
-        printProperties(classpathProperties);
-
-        // 从文件系统加载 YAML 文件
-        String filesystemYamlPath = "/suyh/github/java-demo-springboot/demo-64-springboot_properties_yaml/demo64-01-load/src/main/resources/suyh_properties.properties";
-        Properties filesystemProperties = loadYamlProperties(filesystemYamlPath);
-        printProperties(filesystemProperties);
-    }
-
-    private static Properties loadYamlProperties(String yamlFilePath) {
-        YamlPropertiesFactoryBean yamlFactory = new YamlPropertiesFactoryBean();
-        Resource resource;
-
-        if (yamlFilePath.startsWith("classpath:")) {
-            resource = new ClassPathResource(yamlFilePath.substring("classpath:".length()));
-        } else {
-            resource = new FileSystemResource(yamlFilePath);
+        {
+            // 从类路径加载 YAML 文件
+            YamlPropertiesFactoryBean yamlFactory = new YamlPropertiesFactoryBean();
+            Resource resource = new ClassPathResource("/suyh_properties.properties");
+            yamlFactory.setResources(resource);
+            Properties classpathProperties = yamlFactory.getObject();
+            assert classpathProperties != null;
+            printProperties(classpathProperties);
         }
 
-        yamlFactory.setResources(resource);
-        return yamlFactory.getObject();
+        {
+            // 从文件系统加载 YAML 文件
+            String yamlFilePath = "/suyh/github/java-demo-springboot/demo-64-springboot_properties_yaml/demo64-01-load/src/main/resources/suyh_properties.properties";
+
+            YamlPropertiesFactoryBean yamlFactory = new YamlPropertiesFactoryBean();
+            Resource resource = new FileSystemResource(yamlFilePath);
+
+            yamlFactory.setResources(resource);
+            Properties filesystemProperties = yamlFactory.getObject();
+            assert filesystemProperties != null;
+            printProperties(filesystemProperties);
+        }
     }
 
     private static void printProperties(Properties properties) {
