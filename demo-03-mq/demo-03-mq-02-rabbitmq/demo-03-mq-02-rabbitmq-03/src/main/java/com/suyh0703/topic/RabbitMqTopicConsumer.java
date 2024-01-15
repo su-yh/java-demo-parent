c600f9f7799e2af51f4d2b40d5cc1b4ec8fd2d5e
suyh <suyunhong@ater-info.com>
2024-01-15 14:24:46 +0800

    rmq

---
+++ demo-03-mq/demo-03-mq-02-rabbitmq/demo-03-mq-02-rabbitmq-03/src/main/java/com/suyh0703/topic/RabbitMqTopicConsumer.java
@@ -0,0 +1,48 @@
+package com.suyh0703.topic;
+
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.CancelCallback;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @author suyh
+ * @since 2023-12-21
+ */
+public class RabbitMqTopicConsumer {
+    private final static String EXCHANGE_NAME = "flink_output_topic_exchange";
+    private final static String ROUTING_KEY = "cohort_key_1";
+    private final static String QUEUE_NAME = "suyhQueueName_2";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("192.168.8.34");
+        factory.setUsername("admin");
+        factory.setPassword("aiteer");
+        factory.setVirtualHost("/flinkhost");
+
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.TOPIC, true, false, null);
+        channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+        channel.queueBind(QUEUE_NAME, EXCHANGE_NAME, ROUTING_KEY);
+
+        System.out.println("等待接收消息...");
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String correlationId = delivery.getProperties().getCorrelationId();
+            String message = new String(delivery.getBody());
+            System.out.println("接收的消息(" + EXCHANGE_NAME + "), routing key: " + ROUTING_KEY + ": " + message + ", correlationId: " + correlationId);
+        };
+
+        CancelCallback cancelCallback = consumerTag -> {
+            System.out.println("消息消费被中断");
+        };
+
+        channel.basicConsume(QUEUE_NAME, true, deliverCallback, cancelCallback);
+    }
+}
---
+++ demo-03-mq/demo-03-mq-02-rabbitmq/demo-03-mq-02-rabbitmq-03/src/main/java/com/suyh0703/topic/RabbitMqTopicProducer.java
@@ -0,0 +1,49 @@
+package com.suyh0703.topic;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @author suyh
+ * @since 2023-12-21
+ */
+public class RabbitMqTopicProducer {
+    private final static String EXCHANGE_NAME = "flink_output_topic_exchange";
+    private final static String COHORT_KEY_1 = "cohort_key_1";
+    private final static String RETENTION_KEY = "paid_retention_key";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("192.168.8.34");
+        factory.setUsername("admin");
+        factory.setPassword("aiteer");
+        factory.setVirtualHost("/flinkhost");
+
+        try (Connection connection = factory.newConnection(); Channel channel = connection.createChannel()) {
+            channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.TOPIC, true, false, null);
+            String message = "hello world";
+
+            /*
+             * 发送一个消息
+             * 1. 发送到哪个交换机
+             * 2. 路由的key 是哪个
+             * 3. 其他的参数信息
+             * 4. 发送消息的消息体
+             */
+            AMQP.BasicProperties properties = new AMQP.BasicProperties();
+            properties = properties.builder().correlationId(UUID.randomUUID().toString()).build();
+            channel.basicPublish(EXCHANGE_NAME, COHORT_KEY_1, properties, message.getBytes(StandardCharsets.UTF_8));
+            channel.basicPublish(EXCHANGE_NAME, RETENTION_KEY, properties, message.getBytes(StandardCharsets.UTF_8));
+            System.out.println("消息发送完毕");
+        }
+
+    }
+}
