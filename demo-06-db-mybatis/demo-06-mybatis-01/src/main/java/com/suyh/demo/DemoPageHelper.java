cc7c5b2f7f97b50625d51c79ba177bbe49c55997
suyh <suyunhong@ater-info.com>
2024-03-07 11:15:49 +0800

    分页工具

--- demo-06-db-mybatis/demo-06-mybatis-01/src/main/java/com/suyh/demo/DemoPageHelper.java
+++ demo-06-db-mybatis/demo-06-mybatis-01/src/main/java/com/suyh/demo/DemoPageHelper.java
@@ -26,6 +26,9 @@ public class DemoPageHelper {
         boolean total = true;   // 是否需要记录总条数
         PageHelper.startPage(currentPage, pageSize, total);
         // 返回的list 就是实际获取互的条数，最多为pageSize
+        // 这里需要注意，这个框架中间不能有其他的查询语句，否则记录总条数就会取不到。
+        // 因为似乎只有第一条查询出来的结果才会是Page<?> 对象实例，而总条数就是Page#getTotal() 方法里记录着。
+        // 可以直接转到 Page<?> 再调用getTotal() 方法
         List<CrmContactsInfo> crmContactsInfos = crmContactsInfoMapper.selectAll();// 去查询数据库，不需要在SQL 语句中添加LIMIT 相关的语句
         PageInfo<CrmContactsInfo> info = new PageInfo<>(crmContactsInfos);
         long totalSize = info.getTotal();   // 当前查询条件所获取到的所有数据的总条数。
