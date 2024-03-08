bc94f912eb53ab4d3b8001f64323e21e864e1006
suyh <suyunhong@ater-info.com>
2024-03-08 18:41:58 +0800

    spring security

--- demo-61-security/demo-61-suyh-security/src/main/java/com/suyh/security/controller/LoginAfterController.java
+++ demo-61-security/demo-61-suyh-security/src/main/java/com/suyh/security/controller/LoginAfterController.java
@@ -5,8 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-
 /**
  * 认证后置处理器
  *
@@ -15,16 +13,24 @@ import javax.servlet.http.HttpServletRequest;
  */
 @RestController
 @RequestMapping("/login/after")
-public class LoginPostController {
+public class LoginAfterController {
 
-    // 该接口由spring security 自动调用，在用户通过了帐号密码校验之后，会来访问该接口。不过需要自行配置，在WebSecurityConfigurerAdapter 中。
+    // 该接口由spring security 自动调用，在用户通过了帐号密码校验之后，会来访问该接口。
+    // 不过需要自行配置，在WebSecurityConfigurerAdapter 中。
+    // 这种方式不好，因为重定向后被改成了GET 方法，同时参数全都丢失了，虽然给补充了一个Authentication 参数。
     @RequestMapping(value = "/successful", method = {RequestMethod.GET})
+    @Deprecated
     public Object loginSuccessful(Authentication authentication) {
         return authentication.getPrincipal();
     }
 
+    // TODO: suyh - 这里的问题主要是没有Authentication，有空的时候看下 SavedRequestAwareAuthenticationSuccessHandler  是怎么把这些参数补充上的
+    // 不过这里保留了登录时的原始参数。
     @RequestMapping(value = "/successful", method = RequestMethod.POST)
-    public Object loginSuccessfulP(HttpServletRequest request) {
+    public Object loginSuccessfulP(String username, String password, Integer code) {
+        System.out.println("username: " + username);
+        System.out.println("password: " + password);
+        System.out.println("code: " + code);
         return "SUCCESSFUL";
     }
 
