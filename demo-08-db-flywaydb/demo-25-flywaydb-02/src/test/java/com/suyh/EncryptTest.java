89ec12dd419a868d7a419465fc190ec66b65b493
suyh <su787910081@163.com>
2024-03-19 22:42:27 +0800

    动态数据源，兼容老配置项。

--- demo-08-db-flywaydb/demo-25-flywaydb-02/src/test/java/com/suyh/EncryptTest.java
+++ demo-08-db-flywaydb/demo-25-flywaydb-02/src/test/java/com/suyh/EncryptTest.java
@@ -1,15 +1,15 @@
-package com.suyh;
-
-import com.suyh.encrypt.PropertiesEncrypt;
-import lombok.extern.slf4j.Slf4j;
-import org.junit.Test;
-
-@Slf4j
-public class EncryptTest {
-    @Test
-    public void test01() {
-        PropertiesEncrypt encrypt = new PropertiesEncrypt();
-        String strEncrypt = encrypt.encrypt("root");
-        log.info("root encrypt result: {}", strEncrypt);
-    }
-}
+//package com.suyh;
+//
+//import com.suyh.encrypt.PropertiesEncrypt;
+//import lombok.extern.slf4j.Slf4j;
+//import org.junit.Test;
+//
+//@Slf4j
+//public class EncryptTest {
+//    @Test
+//    public void test01() {
+//        PropertiesEncrypt encrypt = new PropertiesEncrypt();
+//        String strEncrypt = encrypt.encrypt("root");
+//        log.info("root encrypt result: {}", strEncrypt);
+//    }
+//}
