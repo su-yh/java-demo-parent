7f2d14d0a5eb95a2be5a9dc82b8df20e405a3200
suyh <su_787910081@sina.com>
2024-06-22 18:57:53 +0800

    迁移代码

--- demo-21-06-error-attribute/src/main/java/com/suyh2106/mvc/exception/AbstractException.java
+++
@@ -1,5 +0,0 @@
-package com.suyh2106.mvc.exception;
-
-public abstract class AbstractException extends RuntimeException {
-    private static final long serialVersionUID = -4365987799788427525L;
-}
--- demo-22-validation-01/src/main/java/com/suyh2201/constants/ValidationConstants.java
+++
@@ -1,6 +0,0 @@
-package com.suyh2201.constants;
-
-public class ValidationConstants {
-    public static final int KINDS_SERVLET = 1;
-    public static final int KINDS_REACTIVE = 2;
-}
