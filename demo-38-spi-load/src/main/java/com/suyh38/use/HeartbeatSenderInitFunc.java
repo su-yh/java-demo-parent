77543da818fc8fab91729bc3265e06c523a8493c
suyh <su_787910081@sina.com>
2024-06-23 13:15:46 +0800

    迁移代码

--- demo-38-spi-load/src/main/java/com/suyh38/use/HeartbeatSenderInitFunc.java
+++
@@ -1,8 +0,0 @@
-package com.suyh38.use;
-
-public class HeartbeatSenderInitFunc implements InitFunc {
-    @Override
-    public void init() throws Exception {
-        // do somethine
-    }
-}
--- demo-38-spi-load/src/main/java/com/suyh38/use/InitFunc.java
+++
@@ -1,6 +0,0 @@
-package com.suyh38.use;
-
-public interface InitFunc {
-
-    void init() throws Exception;
-}
