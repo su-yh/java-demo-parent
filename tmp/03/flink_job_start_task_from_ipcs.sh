f11c1987945220ddb960ce638261eb70d0fc40e3
suyh <suyunhong@ater-info.com>
2024-11-23 20:29:54 +0800

    读写消息

--- tmp/03/flink_job_start_task_from_ipcs.sh
+++ tmp/03/flink_job_start_task_from_ipcs.sh
@@ -6,28 +6,48 @@
 #./ipcmqs -w --pns hy --date 20241102
 
 # 1. 查询flink 集群是否空闲
+FLINK_CLUSTER_IDLE=false
+while read line; do
+  if [[ $line =~ "Flink cluster is idle" ]]; then
+    echo "Flink cluster is idle"
+    FLINK_CLUSTER_IDLE=true
+  fi
+done < <(python3 ./flink_cluster_job_info.py)
 
+if [[ ! "${FLINK_CLUSTER_IDLE}x" = "true"x ]]; then
+  echo "Flink cluster is busy"
+  exit 0
+fi
 
+# flink 集群空闲中
 DATES=""
 PNS=""
 CHANNEL_LIST=""
 while read line; do
-    if [[ $line =~ ^dates:\ (.*)$ ]]; then
-        DATES=$(echo ${BASH_REMATCH[1]})
-    elif [[ $line =~ ^pns:\ (.*)$ ]]; then
-        PNS=$(echo ${BASH_REMATCH[1]})
-    elif [[ $line =~ ^channelList:\ (.*)$ ]]; then
-        CHANNEL_LIST=$(echo ${BASH_REMATCH[1]})
-    else
-        echo "不匹配预期格式的行: $line"
-    fi
+  if [[ $line =~ ^dates:\ (.*)$ ]]; then
+    DATES=$(echo ${BASH_REMATCH[1]})
+  elif [[ $line =~ ^pns:\ (.*)$ ]]; then
+    PNS=$(echo ${BASH_REMATCH[1]})
+  elif [[ $line =~ ^channelList:\ (.*)$ ]]; then
+    CHANNEL_LIST=$(echo ${BASH_REMATCH[1]})
+  else
+    echo "不匹配预期格式的行: $line"
+  fi
 done < <(./ipcmqs -r)
-
 
 echo "DATES: ${DATES}, PNS: ${PNS}, CHANNEL_LIST: ${CHANNEL_LIST}"
 
+# 消息队列中没有数据
+if [[ "${DATES}"x = ""x || "${PNS}x" == ""x ]]; then
+  echo "ipcs mq is empty"
+  exit 0
+fi
+
+cd /home/suyunhong/flink/flink-repetition/flink-1.18.0
+./bin/flink run -p  8 -d job-jar/cdap-repetition-job-1.8.0.jar
+cd -
 
 
-TZ='Asia/Shanghai' date +%Y%m%d
-TZ='Asia/Tokyo' date +%Y%m%d
-TZ='Asia/Kolkata' date +%Y%m%d
+#TZ='Asia/Shanghai' date +%Y%m%d
+#TZ='Asia/Tokyo' date +%Y%m%d
+#TZ='Asia/Kolkata' date +%Y%m%d
